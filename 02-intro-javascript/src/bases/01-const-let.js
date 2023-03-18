04adbbc4ba85c50f5cbc79b4fc764d142c84c9a2
Iñaki García <igardiet@gmail.com>
2023-03-18 16:54:03 +0100

    feat: code minor formatting

--- 02-intro-javascript/src/bases/01-const-let.js
+++ 02-intro-javascript/src/bases/01-const-let.js
@@ -1,16 +1,16 @@
 // Variables and Constants
 
-const name = 'Inaki'
-const surname = 'Dietze'
+const name = "Inaki";
+const surname = "Dietze";
 
-let value = 5
-value = 4
+let value = 5;
+value = 4;
 
-console.log(name, surname, value)
+console.log(name, surname, value);
 
 // var is deprecated
 if (true) {
-    const name = 'John'
-    console.log(name)
+  const name = "John";
+  console.log(name);
 }
-console.log(value)
+console.log(value);
