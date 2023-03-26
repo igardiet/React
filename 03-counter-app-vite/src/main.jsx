f14b822f613514e95d2587bd979d2bf305d99ef6
Iñaki García <igardiet@gmail.com>
2023-03-26 13:59:37 +0200

    feat: FirstApp and CounterApp components import

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,9 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { FirstApp } from "./FirstApp";
 import { HelloWorldApp } from "./HelloWorldApp";
+import { CounterApp } from "./CounterApp";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <HelloWorldApp />
+    <CounterApp value={10} />
+    {/* <FirstApp
+    title={'Title'}
+    subTitle={'Sub title'}
+    /> */}
+    {/* sending props to its children */}
+    {/* <HelloWorldApp /> */}
   </React.StrictMode>
 );
