c54f4b7d9a73a4f1a6e724f612e1b6801a93dde3
Iñaki García <igardiet@gmail.com>
2023-03-26 13:56:26 +0200

    feat!: index.js updated to text.txt to note react topics

--- index.js
+++
@@ -1,3 +0,0 @@
-// Componente: pequeña pieza de codigo encapsulada re-utilizable que puede tener estado o no
-// Estado: como se encuentra la info del componente en un punto determinado del tiempo
-// Estado inicial: cuando el componente es renderizado por primera vez, es como se encuentra la info del componente la primera vez que se abre
