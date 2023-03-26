a9d904bf8b33aed0b146843431af60ce807f6495
Iñaki García <igardiet@gmail.com>
2023-03-26 13:58:49 +0200

    feat: file creation

--- 03-counter-app-vite/src/HelloWorldApp.jsx
+++ 03-counter-app-vite/src/HelloWorldApp.jsx
@@ -1,7 +1,20 @@
-// export const HelloWorldApp = () => {
-//     return (
-//         <h1>Hello World App</h1>
-//     )
-// }
+import "./styles.css";
 
-export const HelloWorldApp = () => <h1>Hello World App</h1>
+const newMessage = {
+  message: "Hello World",
+  title: "Title example",
+};
+
+const result = (a, b, c, d) => {
+  return (a * b + c) / d;
+};
+
+export const HelloWorldApp = () => {
+  return (
+    <>
+      <h1>{result(1, 2, 3, 5)}</h1>
+      <code>{JSON.stringify(newMessage)}</code>
+      <p>First paragraph</p>
+    </>
+  );
+};
