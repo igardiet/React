669c7f1c8b7e1af6750d088a2e091daa00b10a51
Iñaki García <igardiet@gmail.com>
2023-03-27 15:25:50 +0200

    feat: counter handle add, substract and reset functions

--- 03-counter-app-vite/src/CounterApp.jsx
+++ 03-counter-app-vite/src/CounterApp.jsx
@@ -1,10 +1,20 @@
 import PropTypes from "prop-types";
+import { useState } from "react";
 
 export const CounterApp = ({ value }) => {
+  const [counter, setCounter] = useState(value);
+
+  const handleAdd = () => setCounter(counter + 1);
+  const handleSubtract = () => setCounter(counter - 1);
+  const handleReset = () => setCounter(value);
+
   return (
     <>
       <h1>CounterApp</h1>
-      <h2>{value}</h2>
+      <h2>{counter}</h2>
+      <button onClick={handleAdd}>+1</button>
+      <button onClick={handleSubtract}>-1</button>
+      <button onClick={handleReset}>Reset</button>
     </>
   );
 };
