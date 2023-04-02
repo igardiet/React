c53dfec0cc9d6fc20a0b50641d4367b267d69dbb
Iñaki García <igardiet@gmail.com>
2023-04-02 15:18:41 +0200

    code cleaning for tests

--- 03-counter-app-vite/src/base-pruebas/11-async-await.js
+++ 03-counter-app-vite/src/base-pruebas/11-async-await.js
@@ -1,32 +1,17 @@
+export const getImagen = async () => {
+  try {
+    const apiKey = "sIDMzSvljuCLA3L1eySGs5cSyITyE8vX";
+    const resp = await fetch(
+      `http://api.giphy.com/v1/gifs/random?api_key=${apiKey}`
+    );
+    const { data } = await resp.json();
 
+    const { url } = data.images.original;
+  } catch (error) {
+    // manejo del error
+    console.error(error);
+    return "Image not found!";
+  }
+};
 
-// const getImagenPromesa = () => new Promise( resolve => resolve('https://ajskdhaskjdhajs.com') )
-// getImagenPromesa().then( console.log );
-
-const getImagen = async() => {
-
-    try {
-
-        const apiKey = 'C1khQe3Z7R1W2lfTO9myKeuShdqFYSGC';
-        const resp   = await fetch(`http://api.giphy.com/v1/gifs/random?api_key=${ apiKey }`);
-        const { data } = await resp.json(); 
-
-        const { url } = data.images.original;
-
-        const img = document.createElement('img');
-        img.src = url;
-        document.body.append( img );
-
-    } catch (error) {
-        // manejo del error
-        console.error(error)
-    }
-    
-    
-    
-}
-
- getImagen();
-
-
-
+getImagen();
