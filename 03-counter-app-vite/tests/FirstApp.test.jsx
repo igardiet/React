760d1a8d28d334424a9322296779f7b356352637
Iñaki García <igardiet@gmail.com>
2023-04-02 19:58:44 +0200

    FirstApp test variants

--- 03-counter-app-vite/tests/FirstApp.test.jsx
+++ 03-counter-app-vite/tests/FirstApp.test.jsx
@@ -5,6 +5,14 @@ import { FirstApp } from "../src/FirstApp";
 describe("Pruebas en <FirstApp />", () => {
   test("debe de hacer match con el snapshot", () => {
     const title = "Hola, soy Goku";
-    render(<FirstApp title={title} />);
+    const { container } = render(<FirstApp title={title} />);
+    expect(container).toMatchSnapshot();
+  });
+  test("debe de mostrar el título en un h1", () => {
+    const title = "Hola, soy Goku";
+    const { container, getByText } = render(<FirstApp title={title} />);
+    expect(getByText(title)).toBeTruthy();
+    const h1 = container.querySelector("h1");
+    expect(h1.innerHTML).toContain(title);
   });
 });
