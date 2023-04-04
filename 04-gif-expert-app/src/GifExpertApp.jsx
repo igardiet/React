ac2f9b89ca58004afa3073ea23553721ccb73ab5
Iñaki García <igardiet@gmail.com>
2023-04-04 17:24:45 +0200

    feat: AddCategory and GifGrid imports, handleAddCategory function logic

--- 04-gif-expert-app/src/GifExpertApp.jsx
+++ 04-gif-expert-app/src/GifExpertApp.jsx
@@ -1,21 +1,23 @@
 import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
 
 export const GifExpertApp = () => {
-  const [categories, setCategories] = useState(["One Punch", "Dragon Ball"]);
+  const [categories, setCategories] = useState(['Naruto']);
 
-  const onAddCategory =() => {
-    setCategories('lala')
-  }
+  const hanldeAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+    setCategories([newCategory, ...categories]);
+    // setCategories((cat) => ["Valorant", ...cat]);
+  };
 
   return (
     <>
       <h1>GifExpertApp</h1>
-      <button onClick={onAddCategory}>Add</button>
-      <ol>
-        {categories.map(category => {
-          return <li key={category}>{category}</li>;
-        })}
-      </ol>
+      <AddCategory onNewCategory={hanldeAddCategory} />
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
     </>
   );
 };
