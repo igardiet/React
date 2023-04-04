5d19817d6236bb591149f567d52185db21e3d085
Iñaki García <igardiet@gmail.com>
2023-04-04 23:23:50 +0200

    feat: custom hook import and its ternary operator, GifItem import

--- 04-gif-expert-app/src/components/GifGrid.jsx
+++ 04-gif-expert-app/src/components/GifGrid.jsx
@@ -1,12 +1,20 @@
-import { getGifs } from "../helpers/getGifs";
+import { GifItem } from "./GifItem";
+import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export const GifGrid = ({ category }) => {
-  getGifs(category);
-};
+  const { images, isLoading } = useFetchGifs(category);
 
-return (
-  <>
-    <h3>{category}</h3>
-    <p>Hola Mundd</p>
-  </>
-);
+  return (
+    <>
+      <h3>{category}</h3>
+
+      {isLoading && <h2>Loading...</h2>}
+
+      <div className="card-grid">
+        {images.map((image) => (
+          <GifItem key={image.id} {...image} />
+        ))}
+      </div>
+    </>
+  );
+};
