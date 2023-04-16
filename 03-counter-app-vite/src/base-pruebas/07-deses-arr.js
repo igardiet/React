a264be0a11f826d8fe8e0655c26b1c2c17d7bae0
Iñaki García <igardiet@gmail.com>
2023-04-16 12:15:45 +0200

    file moved to react-fh-course

--- 03-counter-app-vite/src/base-pruebas/07-deses-arr.js
+++
@@ -1,4 +0,0 @@
-
-export const retornaArreglo = () =>{
-    return ['ABC', 123];
-}
--- 04-gif-expert-app/src/components/GifItem.jsx
+++
@@ -1,8 +0,0 @@
-export const GifItem = ({ id, title, url }) => {
-  return (
-    <div className="card" key={id}>
-      <img src={url} alt={title} />
-      <p>{title}</p>
-    </div>
-  );
-};
