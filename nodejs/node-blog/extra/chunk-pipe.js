7d6fc908c40e05c791809b67259ad94db98198ab
Iñaki García <igardiet@gmail.com>
2023-04-16 12:21:21 +0200

    file comes from nodejs repository and inserted in nodejs/node-blog/extra folder

---
+++ nodejs/node-blog/extra/chunk-pipe.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+const readStream = fs.createReadStream('./docs/blog3.txt', {
+  encoding: 'utf8',
+});
+const writeStream = fs.createWriteStream('./docs/blog4.txt');
+
+// CHUNK OR NO-PIPING
+readStream.on('data', (chunk) => {
+  console.log('----- NEW CHUNK -----');
+  console.log(chunk);
+  writeStream.write('\nNEW CHUNK\n');
+  writeStream.write(chunk);
+});
+
+// PIPING
+readStream.pipe(writeStream);
