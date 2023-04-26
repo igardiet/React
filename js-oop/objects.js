0ceea1f5e73d3da4781692e2bfe8a278013c5058
Iñaki García <igardiet@gmail.com>
2023-04-26 22:36:09 +0200

    file first commit

---
+++ js-oop/objects.js
@@ -0,0 +1,13 @@
+// The constructor function creates the new object
+// the NEW keyword:
+// 1. creates a new empty object {}
+// 2. set the value of 'this' to be the new empty object
+// 3. calls the constructor method
+
+class Person {
+    constructor(){
+        
+    }
+}
+
+const firstPerson = new Person()
