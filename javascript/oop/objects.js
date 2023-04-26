a9908ea88c7baf2139bbc1520e169c847c01292b
Iñaki García <igardiet@gmail.com>
2023-04-26 23:08:59 +0200

    login, logout, updateNumber functions

--- javascript/oop/objects.js
+++ javascript/oop/objects.js
@@ -5,11 +5,27 @@
 // 3. calls the constructor method
 
 class Person {
-    constructor(name, surname){
-        this.name = name
-        this.surname = surname
-    }
+  constructor(name, surname) {
+    this.name = name;
+    this.surname = surname;
+    this.number = 0;
+  }
+  login() {
+    console.log(this.name, 'Has logged in!');
+    return this;
+  }
+  logout() {
+    console.log(this.name, 'Has logged out!');
+    return this;
+  }
+  updateNumber() {
+    this.number++;
+    console.log(this.name, 'number is now', this.number);
+    return this;
+  }
 }
 
-const firstPerson = new Person('Bob', 'Dylan')
-const secondPerson = new Person('Lucky', 'Strike')
+const firstPerson = new Person('Bob', 'Dylan');
+const secondPerson = new Person('Lucky', 'Strike');
+
+firstPerson.login().updateNumber().updateNumber().logout();
