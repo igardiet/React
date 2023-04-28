21e474611185e01405193d2a49f4ca613b35b4f7
Iñaki García <igardiet@gmail.com>
2023-04-28 21:23:10 +0200

    boolean

---
+++ c/bool.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main()
+{
+	bool blackIsDark = true;
+	printf("Is black color dark (1 is yes, 0 is no)? %i\n ", blackIsDark);
+	printf("%i\n", blackIsDark + 10);
+	return 0;
+}
