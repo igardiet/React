0d6f78f2d176c3093dc9e218530e76311cba2789
Iñaki García <igardiet@gmail.com>
2023-04-29 14:10:05 +0200

    booklist and navbar component import

--- javascript/mern/react/react-context-and-hooks/src/App.jsx
+++ javascript/mern/react/react-context-and-hooks/src/App.jsx
@@ -1,9 +1,12 @@
+import BookList from './components/BookList'
+import Navbar from './components/Navbar'
 
 export const App = () => {
 
   return (
-    <>
-      
-    </>
+    <div className='App'>
+      <Navbar />
+      <BookList />
+    </div>
   )
 }
