581c62949afad708407f30d5d3db1aa90eab5324
Iñaki García <igardiet@gmail.com>
2023-04-30 21:29:57 +0200

    Form component

---
+++ javascript/react/react-context-and-hooks/hooks/src/components/NewSongForm.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+
+export const NewSongForm = ({ addSong }) => {
+  const [title, setTitle] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addSong(title);
+    setTitle('');
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>Song name:</label>
+      <input
+        type='text'
+        value={title}
+        required
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input type='submit' value='Add song' />
+    </form>
+  );
+};
