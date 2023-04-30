5bd8b5b00de104d6f576dde299562a3af7647a6c
Iñaki García <igardiet@gmail.com>
2023-04-30 15:04:25 +0200

    theme setState condition

--- javascript/react/react-context-and-hooks/src/contexts/ThemeContext.jsx
+++ javascript/react/react-context-and-hooks/src/contexts/ThemeContext.jsx
@@ -8,9 +8,12 @@ export default class ThemeContextProvider extends Component {
     light: { syntax: '#555', ui: '#ddd', bg: '#eee' },
     dark: { syntax: '#ddd', ui: '#333', bg: '#555' },
   };
+  theme = () => {
+    this.setState({ isLightTheme: !this.state.isLightTheme });
+  };
   render() {
     return (
-      <ThemeContext.Provider value={{ ...this.state }}>
+      <ThemeContext.Provider value={{ ...this.state, theme: this.theme }}>
         {this.props.children}
       </ThemeContext.Provider>
     );
