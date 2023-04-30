023e83ee46ac288e5a4411befbe8b88c2ffd2c58
Iñaki García <igardiet@gmail.com>
2023-04-30 20:50:52 +0200

    useState array of objects and songs map loop

--- javascript/react/react-context-and-hooks/hooks/src/components/SongList.jsx
+++ javascript/react/react-context-and-hooks/hooks/src/components/SongList.jsx
@@ -1,10 +1,23 @@
+import { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+
 export const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: 'song one', id: 1 },
+    { title: 'song two', id: 2 },
+    { title: 'song three', id: 3 },
+  ]);
+  const addSong = () => {
+    setSongs([...songs, { title: 'new song', id: uuidv4() }]);
+  };
   return (
     <div className='song-list'>
       <ul>
-        <li>this wild darkness</li>
-        <li>memory gospel</li>
+        {songs.map((song) => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
       </ul>
+      <button onClick={addSong}>Add song!</button>
     </div>
   );
 };
