31198226ff0e99a0bd173a74d94a4b6afb3af2bc
Iñaki García <igardiet@gmail.com>
2023-05-01 22:15:51 +0200

    import BookContext and books map

--- javascript/react/react-context-and-hooks/context/src/components/BookList.jsx
+++ javascript/react/react-context-and-hooks/context/src/components/BookList.jsx
@@ -3,6 +3,7 @@ import {
   useContext,
 } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
+import { BookContext } from '../contexts/BookContext';
 
 // OLD CLASS COMPONENT VERSION
 // export default class BookList extends Component {
@@ -28,6 +29,7 @@ import { ThemeContext } from '../contexts/ThemeContext';
 // USECONTEXT INSIDE FUNCTIONAL COMPONENTS
 export const BookList = () => {
   const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { books } = useContext(BookContext);
   const theme = isLightTheme ? light : dark;
   return (
     <div
@@ -35,9 +37,13 @@ export const BookList = () => {
       style={{ color: theme.syntax, background: theme.bg }}
     >
       <ul>
-        <li style={{ background: theme.ui }}>the way of kings</li>
-        <li style={{ background: theme.ui }}>the name of the wind</li>
-        <li style={{ background: theme.ui }}>the final empire</li>
+        {books.map((book) => {
+          return (
+            <li key={book.id} style={{ background: theme.ui }}>
+              {book.title}
+            </li>
+          );
+        })}
       </ul>
     </div>
   );
