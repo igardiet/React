005f1fb646b364fa5b58c6efa312885735f5b2c8
Iñaki García <igardiet@gmail.com>
2023-05-01 22:00:17 +0200

    old class component vs. usecontext inside functional components

--- javascript/react/react-context-and-hooks/context/src/components/ThemeToggle.jsx
+++ javascript/react/react-context-and-hooks/context/src/components/ThemeToggle.jsx
@@ -1,10 +1,19 @@
-import { Component } from 'react';
+import {
+  // Component,
+  useContext,
+} from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 
-export default class ThemeToggle extends Component {
-  static contextType = ThemeContext;
-  render() {
-    const { theme } = this.context;
-    return <button onClick={theme}>Theme toggle</button>;
-  }
-}
+// OLD CLASS COMPONENT VERSION
+// export default class ThemeToggle extends Component {
+//   static contextType = ThemeContext;
+//   render() {
+//     const { theme } = this.context;
+//   }
+// }
+
+// USECONTEXT INSIDE FUNCTIONAL COMPONENTS
+export const ThemeToggle = () => {
+  const { theme } = useContext(ThemeContext);
+  return <button onClick={theme}>Theme toggle</button>;
+};
