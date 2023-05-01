7f0e8ed9fec0d92d91654eb8b01ef26c870be291
Iñaki García <igardiet@gmail.com>
2023-05-01 21:59:47 +0200

    old class component version vs. usecontext inside functional components

--- javascript/react/react-context-and-hooks/context/src/components/Navbar.jsx
+++ javascript/react/react-context-and-hooks/context/src/components/Navbar.jsx
@@ -1,34 +1,42 @@
-import { Component } from 'react';
+// import { Component } from 'react';
 import { ThemeContext } from '../contexts/ThemeContext';
 import { AuthContext } from '../contexts/AuthContext';
 
-export default class Navbar extends Component {
-  render() {
-    return (
-      <AuthContext.Consumer>
-        {(authContext) => (
-          <ThemeContext.Consumer>
-            {(themeContext) => {
-              const { isAuthenticated, toggleAuth } = authContext;
-              const { isLightTheme, light, dark } = themeContext;
-              const theme = isLightTheme ? light : dark;
-              return (
-                <nav style={{ background: theme.ui, color: theme.syntax }}>
-                  <h1>Context App</h1>
-                  <div onClick={toggleAuth}>
-                    {isAuthenticated ? 'Logged in' : 'Logged out'}
-                  </div>
-                  <ul>
-                    <li>Home</li>
-                    <li>About</li>
-                    <li>Contact</li>
-                  </ul>
-                </nav>
-              );
-            }}
-          </ThemeContext.Consumer>
-        )}
-      </AuthContext.Consumer>
-    );
-  }
-}
+// OLD CLASS COMPONENT VERSION
+// export default class Navbar extends Component {
+//   render() {
+//     return (
+//       <AuthContext.Consumer>
+//         {(authContext) => (
+//           <ThemeContext.Consumer>
+//             {(themeContext) => {
+//               const { isAuthenticated, toggleAuth } = authContext;
+//               const { isLightTheme, light, dark } = themeContext;
+//               const theme = isLightTheme ? light : dark;
+//             }}
+//           </ThemeContext.Consumer>
+//         )}
+//       </AuthContext.Consumer>
+//     );
+//   }
+// }
+
+// USECONTEXT INSIDE FUNCTIONAL COMPONENTS
+export const Navbar = () => {
+  const { isLightTheme, light, dark } = useContext(ThemeContext);
+  const { isAuthenticated, toggleAuth } = useContext(AuthContext);
+  const theme = isLightTheme ? light : dark;
+  return (
+    <nav style={{ background: theme.ui, color: theme.syntax }}>
+      <h1>Context App</h1>
+      <div onClick={toggleAuth}>
+        {isAuthenticated ? 'Logged in' : 'Logged out'}
+      </div>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+};
