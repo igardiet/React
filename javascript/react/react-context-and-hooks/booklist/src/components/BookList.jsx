8e815644d4316e2ccd7c9439c1177eb35d6608df
Iñaki García <igardiet@gmail.com>
2023-05-02 22:34:43 +0200

    BookList component condition about book length

---
+++ javascript/react/react-context-and-hooks/booklist/src/components/BookList.jsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { BookDetails } from './BookDetails';
+import { BookContext } from '../contexts/BookContext';
+
+export const BookList = () => {
+  const { books } = useContext(BookContext);
+  return books.length ? (
+    <div className='book-list'>
+      <ul>
+        {books.map((book) => {
+          return <BookDetails book={book} key={book.id} />;
+        })}
+      </ul>
+    </div>
+  ) : (
+    <div className='empty'>No books to read!</div>
+  );
+};
