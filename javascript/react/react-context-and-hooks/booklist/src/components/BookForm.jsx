aa2eb73e5af5fba1975b0ee499dd95185a36f91a
Iñaki García <igardiet@gmail.com>
2023-05-02 22:56:58 +0200

    jsx form => title, author and submit inputs

--- javascript/react/react-context-and-hooks/booklist/src/components/BookForm.jsx
+++ javascript/react/react-context-and-hooks/booklist/src/components/BookForm.jsx
@@ -1,16 +1,30 @@
-
 export const BookForm = () => {
-    const {addBook} = useContext(BookContext)
-    const [title, setTitle] = useState('')
-    const [author, setAuthor] = useState('')
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        addBook(title, author)
-        setTitle('')
-        setAuthor('')
-    }
-
+  const { addBook } = useContext(BookContext);
+  const [title, setTitle] = useState('');
+  const [author, setAuthor] = useState('');
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addBook(title, author);
+    setTitle('');
+    setAuthor('');
+  };
   return (
-    <div>BookForm</div>
-  )
-}
+    <form onSubmit={handleSubmit}>
+      <input
+        type='text'
+        placeholder='Book title'
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input
+        type='text'
+        placeholder='Author'
+        value={author}
+        onChange={(e) => setTitle(e.target.value)}
+        required
+      />
+      <input type='submit' value='Add book!' />
+    </form>
+  );
+};
