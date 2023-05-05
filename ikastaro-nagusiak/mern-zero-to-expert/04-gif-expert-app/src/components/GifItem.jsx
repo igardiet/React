bf26f8ef8737eb8223e9211056299c1a8c17a2f0
Iñaki García <igardiet@gmail.com>
2023-05-05 14:03:05 +0200

    PropTypes import and chained to GifItem

--- ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/src/components/GifItem.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/src/components/GifItem.jsx
@@ -1,3 +1,5 @@
+import PropTypes from 'prop-types'
+
 export const GifItem = ({ id, title, url }) => {
   return (
     <div className="card" key={id}>
@@ -6,3 +8,7 @@ export const GifItem = ({ id, title, url }) => {
     </div>
   );
 };
+GifItem.propTypes = {
+  title: PropTypes.string.isRequired,
+  url: PropTypes.string.isRequired
+}
