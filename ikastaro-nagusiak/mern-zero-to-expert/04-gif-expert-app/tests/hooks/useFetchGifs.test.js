26ebd7dd806c17fe6a5461cc915cbfa303335ded
Iñaki García <igardiet@gmail.com>
2023-05-06 17:54:03 +0200

    useFetchGifs hook tests; return the initial state test

--- ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/hooks/useFetchGifs.test.js
+++ ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/hooks/useFetchGifs.test.js
@@ -2,7 +2,7 @@
  * @jest-environment jsdom
  */
 
-import { renderHook } from '@testing-library/react';
+import { renderHook, waitFor } from '@testing-library/react';
 import { useFetchGifs } from '../../src/hooks/useFetchGifs';
 
 describe('Tests in hook useFetchGifs', () => {
@@ -12,5 +12,16 @@ describe('Tests in hook useFetchGifs', () => {
 
     expect(images.length).toBe(0);
     expect(isLoading).toBeTruthy();
+  });
+
+  test('It must return the initial state', async () => {
+    const { result } = renderHook(() => useFetchGifs('Naruto'));
+
+    await waitFor(() =>
+      expect(result.current.images.length).toBeGreaterThan(0)
+    );
+    const { images, isLoading } = result.current;
+    expect(images.length).toBeGreaterThan(0);
+    expect(isLoading).toBeFalsy();
   });
 });
