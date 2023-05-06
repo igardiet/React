9bbc0f651606a58379a1e0796e49f054a290dbe4
Iñaki García <igardiet@gmail.com>
2023-05-06 14:19:49 +0200

    import 'whatwg-fetch' to fix test error

--- ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/src/helpers/getGifs.js
+++ ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/src/helpers/getGifs.js
@@ -1,3 +1,5 @@
+import 'whatwg-fetch';
+
 export const getGifs = async (category) => {
   const url = `https://api.giphy.com/v1/gifs/search?api_key=ktsNbf0e9qaBT222TyA4VcSxgNVbRJLf&q=${category}&limit=10`;
   const answer = await fetch(url);
