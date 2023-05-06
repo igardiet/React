2fd26273c29afa874b6ae5a80b4bb91fe8b4d2c7
Iñaki García <igardiet@gmail.com>
2023-05-06 16:14:59 +0200

    tests in GifGrid: Show the initial load

--- ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -1,1 +1,15 @@
-describe('first', () => { second })
+/**
+ * @jest-environment jsdom
+ */
+
+const { render, screen } = require('@testing-library/react');
+import { GifGrid } from '../../src/components/GifGrid';
+
+describe('Tests in <GifGrid />', () => {
+  const category = 'Naruto';
+  test('It must show the loading initially', () => {
+    render(<GifGrid category={category} />);
+    expect(screen.getByText('Loading...'));
+    expect(screen.getByText(category));
+  });
+});
