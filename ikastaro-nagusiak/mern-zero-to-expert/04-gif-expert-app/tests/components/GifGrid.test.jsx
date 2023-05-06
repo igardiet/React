5ed08ac1fe36181765cb19f83442ebc5b1b65799
Iñaki García <igardiet@gmail.com>
2023-05-06 17:32:29 +0200

    useFetchGifs jest mock and images load test;

--- ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/components/GifGrid.test.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/04-gif-expert-app/tests/components/GifGrid.test.jsx
@@ -2,14 +2,41 @@
  * @jest-environment jsdom
  */
 
-const { render, screen } = require('@testing-library/react');
+import { render, screen } from '@testing-library/react';
 import { GifGrid } from '../../src/components/GifGrid';
+import { useFetchGifs } from '../../src/hooks/useFetchGifs';
+
+jest.mock('../../src/hooks/useFetchGifs');
 
 describe('Tests in <GifGrid />', () => {
   const category = 'Naruto';
   test('It must show the loading initially', () => {
+    useFetchGifs.mockReturnValue({
+      images: [],
+      isLoading: true,
+    });
     render(<GifGrid category={category} />);
     expect(screen.getByText('Loading...'));
     expect(screen.getByText(category));
+  });
+  test('It must show items when useFetchGifs images load', () => {
+    const gifs = [
+      {
+        id: 'ABC',
+        title: 'Naruto',
+        url: 'https://localhost/naruto.jpg',
+      },
+      {
+        id: '123',
+        title: 'Vegeta',
+        url: 'https://localhost/vegeta.jpg',
+      },
+    ];
+    useFetchGifs.mockReturnValue({
+      images: gifs,
+      isLoading: true,
+    });
+    render(<GifGrid category={category} />);
+    expect(screen.getAllByRole('img').length).toBe(2);
   });
 });
