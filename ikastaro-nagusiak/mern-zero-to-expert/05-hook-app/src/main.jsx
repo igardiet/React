07d3092549645d92fc89497af5ee17dcc6ad1c19
Iñaki García <igardiet@gmail.com>
2023-05-08 16:28:27 +0200

    CounterWithCustomHook component import

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/main.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/main.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { CounterApp } from './useState/CounterApp';
+// import { CounterApp } from './useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 import './index.css';
+import { CounterWithCustomHook } from './useState/CounterWithCustomHook';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <CounterApp />
+    <CounterWithCustomHook />
   </React.StrictMode>
 );
