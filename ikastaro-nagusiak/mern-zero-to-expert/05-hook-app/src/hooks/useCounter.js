26bf4f1d453023689f13276c9275cc6edd557c71
Iñaki García <igardiet@gmail.com>
2023-05-08 16:50:47 +0200

    increment, decrement and reset functions

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/hooks/useCounter.js
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/hooks/useCounter.js
@@ -3,8 +3,23 @@ import { useState } from 'react';
 export const useCounter = (initialValue = 0) => {
   const [counter, setCounter] = useState(initialValue);
 
+  const increment = (value = 1) => {
+    setCounter(counter + value);
+  };
+
+  const decrement = (value = 1) => {
+    if (counter === 0) return; // carrito / cart
+    setCounter(counter - value);
+  };
+
+  const reset = () => {
+    setCounter(initialValue);
+  };
+
   return {
     counter,
-    
-  }
+    increment,
+    decrement,
+    reset,
+  };
 };
