b19e6f867d08aedd0d46101562dc58690c26fa38
Iñaki García <igardiet@gmail.com>
2023-05-08 16:50:26 +0200

    increment, decrement and reset functions by props added to each button

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/useState/CounterWithCustomHook.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/useState/CounterWithCustomHook.jsx
@@ -1,15 +1,27 @@
-import { useCounter } from "../hooks/useCounter";
+import { useCounter } from '../hooks/useCounter';
 
 export const CounterWithCustomHook = () => {
-  const {counter} = useCounter()
+  const { counter, increment, decrement, reset } = useCounter();
 
   return (
     <>
       <h1>Counter with hook: {counter}</h1>
       <hr />
-      <button className='btn btn-primary'>+1</button>
-      <button className='btn btn-primary'>Reset</button>
-      <button className='btn btn-primary'>-1</button>
+      <button
+        className='btn btn-primary btn-danger'
+        onClick={() => increment(1)}
+      >
+        +1
+      </button>
+      <button className='btn btn-primary btn-danger' onClick={reset}>
+        Reset
+      </button>
+      <button
+        className='btn btn-primary btn-danger'
+        onClick={() => decrement(1)}
+      >
+        -1
+      </button>
     </>
   );
 };
