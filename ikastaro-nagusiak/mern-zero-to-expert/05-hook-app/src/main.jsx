30c120f8caadc00d9c1ffb1f7fa83ca912bccd5d
Iñaki García <igardiet@gmail.com>
2023-05-09 23:29:04 +0200

    FormWithCustomHook component import and render

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/main.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/main.jsx
@@ -3,7 +3,10 @@ import ReactDOM from 'react-dom/client';
 // import { CounterApp } from './useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 // import { CounterWithCustomHook } from './useState/CounterWithCustomHook';
-import { SimpleForm } from './useEffect/SimpleForm';
+// import { SimpleForm } from './useEffect/SimpleForm';
+import { FormWithCustomHook } from './useEffect/FormWithCustomHook';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(<SimpleForm />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <FormWithCustomHook />
+);
