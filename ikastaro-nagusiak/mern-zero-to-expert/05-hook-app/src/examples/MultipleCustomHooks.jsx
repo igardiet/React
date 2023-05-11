3897220f3967bcc25ebdefa9472741b4860fb679
Iñaki García <igardiet@gmail.com>
2023-05-11 23:55:21 +0200

    useFetch ternary loading then data rendered

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/examples/MultipleCustomHooks.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/examples/MultipleCustomHooks.jsx
@@ -4,13 +4,25 @@ export const MultipleCustomHooks = () => {
   const { data, isLoading, hasError } = useFetch(
     'https://fakestoreapi.com/products/1'
   );
+  // console.log(data, isLoading, hasError);
 
-  console.log(data, isLoading, hasError);
+  // if (isLoading) {
+  //   return <h2>Loading...</h2>;
+  // }
 
   return (
     <>
       <h1>Breaking Bad Quotes</h1>
       <hr />
+
+      {isLoading ? (
+        <div className='alert alert-info text-center'>Loading...</div>
+      ) : (
+        <blockquote className='blockquote text-end'>
+          <p className='mb-1'>Hello World!</p>
+          <footer className='blockquote-footer'>Lucas Modric</footer>
+        </blockquote>
+      )}
     </>
   );
 };
