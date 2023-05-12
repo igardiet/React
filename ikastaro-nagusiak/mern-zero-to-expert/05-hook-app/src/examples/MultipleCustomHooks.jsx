8e04d91f5991304ba38feef998ddf932c220a53d
Iñaki García <igardiet@gmail.com>
2023-05-13 00:44:11 +0200

    api change; iterable variable; quote and author; next quote button

--- ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/examples/MultipleCustomHooks.jsx
+++ ikastaro-nagusiak/mern-zero-to-expert/05-hook-app/src/examples/MultipleCustomHooks.jsx
@@ -2,27 +2,23 @@ import { useFetch } from '../hooks/useFetch';
 
 export const MultipleCustomHooks = () => {
   const { data, isLoading, hasError } = useFetch(
-    'https://fakestoreapi.com/products/1'
+    'https://api.breakingbadquotes.xyz/v1/quotes/'
   );
-  // console.log(data, isLoading, hasError);
-
-  // if (isLoading) {
-  //   return <h2>Loading...</h2>;
-  // }
+  const { author, quote } = !!data && data[0];
 
   return (
     <>
-      <h1>Breaking Bad Quotes</h1>
+      <h1>Products</h1>
       <hr />
-
       {isLoading ? (
         <div className='alert alert-info text-center'>Loading...</div>
       ) : (
         <blockquote className='blockquote text-end'>
-          <p className='mb-1'>Hello World!</p>
-          <footer className='blockquote-footer'>Lucas Modric</footer>
+          <p className='mb-1'>{quote}</p>
+          <footer className='blockquote-footer'>{author}</footer>
         </blockquote>
       )}
+      <button className='btn btn-primary'>Next Quote</button>
     </>
   );
 };
