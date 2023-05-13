a1a9ae46088c25a744a10b8d3db251d60ecf2e8c
Iñaki García <igardiet@gmail.com>
2023-05-14 00:13:27 +0200

    import FocusScreen and render

--- ziurtagiriak/mern-zero-to-expert/05-hook-app/src/main.jsx
+++ ziurtagiriak/mern-zero-to-expert/05-hook-app/src/main.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './index.css';
 // import { CounterApp } from './useState/CounterApp';
 // import { HooksApp } from './HooksApp';
 // import { CounterWithCustomHook } from './useState/CounterWithCustomHook';
 // import { SimpleForm } from './useEffect/SimpleForm';
 // import { FormWithCustomHook } from './useEffect/FormWithCustomHook';
-import { MultipleCustomHooks } from './examples/MultipleCustomHooks';
-import './index.css';
+// import { MultipleCustomHooks } from './examples/MultipleCustomHooks';
+import { FocusScreen } from './useRef/FocusScreen';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <MultipleCustomHooks />
-);
+ReactDOM.createRoot(document.getElementById('root')).render(<FocusScreen />);
