0c3be094e2a2468f01833e4b2e364d4336a21066
Iñaki García <igardiet@gmail.com>
2023-05-14 00:13:43 +0200

    useRef hook test

---
+++ ziurtagiriak/mern-zero-to-expert/05-hook-app/src/useRef/FocusScreen.jsx
@@ -0,0 +1,26 @@
+import { useRef } from 'react';
+
+export const FocusScreen = () => {
+  const inputRef = useRef();
+  const onClick = () => {
+    // document.querySelector('input').select();
+    // console.log(inputRef);
+    inputRef.current.select();
+  };
+
+  return (
+    <>
+      <h1>Focus Screen</h1>
+      <hr />
+      <input
+        className='form-control'
+        type='text'
+        placeholder='Insert name'
+        ref={inputRef}
+      />
+      <button className='btn btn-primary mt-2' onClick={onClick}>
+        Set Focus
+      </button>
+    </>
+  );
+};
