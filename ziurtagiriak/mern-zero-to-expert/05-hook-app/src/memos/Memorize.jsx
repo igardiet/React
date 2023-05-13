ac39a529ea6be7304042936f6d3ef379024d103d
Iñaki García <igardiet@gmail.com>
2023-05-14 00:30:44 +0200

    file first commit

---
+++ ziurtagiriak/mern-zero-to-expert/05-hook-app/src/memos/Memorize.jsx
@@ -0,0 +1,11 @@
+export const Memorize = () => {
+  return (
+    <>
+      <h1>
+        Couenter: <small>{counter}</small>
+      </h1>
+      <hr />
+      <button className='btn btn-primary' onClick={a}></button>
+    </>
+  );
+};
