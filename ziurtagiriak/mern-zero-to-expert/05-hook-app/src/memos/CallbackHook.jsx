c43d5e5a49006e6bd9216383449b3a426da10b43
Iñaki García <igardiet@gmail.com>
2023-05-14 20:59:08 +0200

    CallbackHook component counter set

--- ziurtagiriak/mern-zero-to-expert/05-hook-app/src/memos/CallbackHook.jsx
+++ ziurtagiriak/mern-zero-to-expert/05-hook-app/src/memos/CallbackHook.jsx
@@ -1,5 +1,25 @@
+import { useCallback, useEffect, useState } from 'react';
+import { ShowIncrement } from './ShowIncrement';
+
 export const CallbackHook = () => {
+  const [counter, setCounter] = useState(0);
+
+  const incrementParent = useCallback((value) => {
+    setCounter((c) => c + value);
+  }, []);
+  useEffect(() => {
+    // incrementParent();
+  }, [incrementParent]);
+
+  //   const incrementParent = () => {
+  //     setCounter(counter + 1)
+  //   }
+
   return (
-    <div>CallbackHook</div>
-  )
-}
+    <>
+      <h1>useCallback Hook: {counter}</h1>
+      <hr />
+      <ShowIncrement increment={incrementParent} />
+    </>
+  );
+};
