8415543949c0ce7c31beebbd11dee3fb1cdbd168
Iñaki García <igardiet@gmail.com>
2023-05-19 21:12:30 +0200

    app first commit

---
+++ ziurtagiriak/node-zero-to-expert/01-hello-world/app2.js
@@ -0,0 +1,15 @@
+console.log('Program start');
+
+setTimeout(() => {
+  console.log('First Timeout');
+}, 3000);
+
+setTimeout(() => {
+  console.log('Secont Timeout');
+}, 0);
+
+setTimeout(() => {
+  console.log('Third Timeout');
+}, 0);
+
+console.log('Program end');
