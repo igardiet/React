c874bafe8caf9cd9d901c38f433661a55f025552
Iñaki García <igardiet@gmail.com>
2023-05-19 22:15:50 +0200

    EXAMPLES: template string before and after; html inside template

---
+++ ziurtagiriak/node-zero-to-expert/02-javascript/template-string.js
@@ -0,0 +1,15 @@
+const named = 'Batman';
+const real = 'Bruce Wayne';
+
+const normal = named + ' is ' + real;
+const template = `${named} is ${real}`;
+
+console.log(normal);
+console.log(template);
+
+console.log(normal === template);
+
+const html = `
+<h1>I'm</h1>
+<p>Batman</p>`;
+console.log(html);
