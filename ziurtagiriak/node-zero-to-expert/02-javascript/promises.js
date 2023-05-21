3c5ce9797baa8d583d9282904b4f74a0cfc7d04f
Iñaki García <igardiet@gmail.com>
2023-05-21 11:39:32 +0200

    FEAT: getEmployee and getSalarycallback functions and its promises

---
+++ ziurtagiriak/node-zero-to-expert/02-javascript/promises.js
@@ -0,0 +1,63 @@
+const employees = [
+  {
+    id: 1,
+    name: 'George',
+  },
+  {
+    id: 2,
+    name: 'John',
+  },
+  {
+    id: 3,
+    name: 'Paul',
+  },
+];
+const salaries = [
+  {
+    id: 1,
+    salary: 1000,
+  },
+  {
+    id: 2,
+    salary: 1500,
+  },
+];
+
+const getEmployee = (id) => {
+  return new Promise((resolve, reject) => {
+    const employee = employees.find((e) => e.id === id)?.name;
+    employee
+      ? resolve(employee)
+      : reject(`There is no employee with an id nº ${id}`);
+  });
+};
+
+const getSalary = (id) => {
+  return new Promise((resolve, reject) => {
+    const salary = salaries.find((s) => s.id === id)?.salary;
+    salary
+      ? resolve(salary)
+      : reject(`There is no salary registered for the id nº ${id}`);
+  });
+};
+
+const id = 1;
+
+getEmployee(id)
+  .then((employee) => console.log(employee))
+  .catch((err) => console.log(err));
+
+getSalary(id)
+  .then((salary) => console.log(salary))
+  .catch((err) => console.log(err));
+
+// DO NOT CODE CALLBACK HELL LIKE THIS!
+// getEmployee(id)
+//   .then((employee) => {
+//     getSalary(id)
+//       .then((salary) => {
+//         console.log(`Employee ${employee} has a salary of ${salary}`);
+//       })
+//       .catch((err) => console.log(err));
+//   })
+//   .catch((err) => console.log(err));
