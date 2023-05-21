2c201f45aec4387137403898d03502bee05894f3
Iñaki García <igardiet@gmail.com>
2023-05-21 22:53:34 +0200

    FEAT: createFile by base promises and code export to respective file

--- ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
@@ -1,22 +1,9 @@
-const fs = require('fs');
+const { createFile } = require('./helpers/multiply');
 
 console.clear();
 
 const base = 5;
 
-console.log('===============');
-console.log(`Table of: ${base}`);
-console.log('===============');
-
-let exit = '';
-
-for (let i = 1; i <= 10; i++) {
-  exit += `${base} * ${i} = ${base * i}\n`;
-}
-
-console.log(exit);
-
-fs.writeFile(`table-${base}.txt`, exit, (err) => {
-  if (err) throw err;
-  console.log(`table-${base}.txt successfully created!`);
-});
+createFile(base)
+  .then((fileName) => console.log(fileName, 'created'))
+  .catch((err) => console.log(err));
