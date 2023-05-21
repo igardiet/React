7943ae6cde4a3ce3b3dc6e1470f76dbd0e462c63
Iñaki García <igardiet@gmail.com>
2023-05-21 12:52:39 +0200

    FEAT: getUserInfo async function and promise call

---
+++ ziurtagiriak/node-zero-to-expert/02-javascript/async-await.js
@@ -0,0 +1,58 @@
+const employees = [
+  {
+    id: 1,
+    name: 'George',
+  },
+  {
+    id: 2,
+    name: 'John',
+  },
+  {
+    id: 3,
+    name: 'Paul',
+  },
+];
+const salaries = [
+  {
+    id: 1,
+    salary: 1000,
+  },
+  {
+    id: 2,
+    salary: 1500,
+  },
+];
+
+const getEmployee = (id) => {
+  return new Promise((resolve, reject) => {
+    const employee = employees.find((e) => e.id === id)?.name;
+    employee
+      ? resolve(employee)
+      : reject(`There is no employee with an id nº ${id}`);
+  });
+};
+
+const getSalary = (id) => {
+  return new Promise((resolve, reject) => {
+    const salary = salaries.find((s) => s.id === id)?.salary;
+    salary
+      ? resolve(salary)
+      : reject(`There is no salary registered for the id nº ${id}`);
+  });
+};
+
+const getUserInfo = async (id) => {
+  try {
+    const employee = await getEmployee(id);
+    const salary = await getSalary(id);
+    return `${employee}'s salary is: $${salary}`;
+  } catch (error) {
+    return error;
+  }
+};
+
+const id = 1;
+
+getUserInfo(id)
+  .then((msg) => console.log(msg))
+  .catch((err) => console.log(err));
