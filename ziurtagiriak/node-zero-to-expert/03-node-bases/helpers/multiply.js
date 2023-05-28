0d32b73def967842205a2e9c981c85a41b40bfd0
Iñaki García <igardiet@gmail.com>
2023-05-28 19:37:18 +0200

    FEAT: list = false parameter in createFile async callback; if list condition to render console.logs of tables

--- ziurtagiriak/node-zero-to-expert/03-node-bases/helpers/multiply.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/helpers/multiply.js
@@ -1,18 +1,19 @@
 const fs = require('fs');
 
-const createFile = async (base = 5) => {
+const createFile = async (base = 5, list = false) => {
   try {
-    console.log('===============');
-    console.log('Table of:', base);
-    console.log('===============');
-
     let exit = '';
 
     for (let i = 1; i <= 10; i++) {
       exit += `${base} * ${i} = ${base * i}\n`;
     }
 
-    console.log(exit);
+    if (list) {
+      console.log('===============');
+      console.log('Table of:', base);
+      console.log('===============');
+      console.log(exit);
+    }
 
     fs.writeFileSync(`table-${base}.txt`, exit);
     return `table-${base}.txt successfully created!`;
