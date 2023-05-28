746e2d993ac9a2e2fd4835cec3baa4ea19762761
Iñaki García <igardiet@gmail.com>
2023-05-28 19:38:34 +0200

    FEAT: base and list options; check if bas is a number; createFile argv.b, argv.l

--- ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
@@ -1,16 +1,34 @@
-// const { createFile } = require('./helpers/multiply');
-const argv = require('yargs').argv;
+const { createFile } = require('./helpers/multiply');
+const argv = require('yargs')
+  .option('b', {
+    alias: 'base',
+    type: 'number',
+    demandOption: true,
+  })
+  .option('l', {
+    alias: 'list',
+    type: 'boolean',
+    demandOption: true,
+    default: false,
+  })
+  .check((argv, options) => {
+    if (isNaN(argv.b)) {
+      throw 'Base must be a number';
+    }
+    return true;
+  }).argv;
 
 console.clear();
 
-console.log(process.argv);
 console.log(argv);
-console.log('base: yargs', argv.base);
+
+createFile(argv.b, argv.l)
+  .then((fileName) => console.log(fileName, 'created'))
+  .catch((err) => console.log(err));
+
+// console.log(process.argv);
+// console.log('base: yargs', argv.b);
 
 // const [, , arg3 = 'base=5'] = process.argv;
 // const [, base = 5] = arg3.split('=');
 // const base = 5;
-
-// createFile(base)
-// .then((fileName) => console.log(fileName, 'created'))
-// .catch((err) => console.log(err));
