0d2b78d21a2ad9fd627fdbfa84bf6bbab18d5d80
Iñaki García <igardiet@gmail.com>
2023-05-28 19:11:05 +0200

    FEAT: yargs required as argv; process. argv, argv.base console logs

--- ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
@@ -1,12 +1,16 @@
-const { createFile } = require('./helpers/multiply');
+// const { createFile } = require('./helpers/multiply');
+const argv = require('yargs').argv;
 
 console.clear();
 
-const [, , arg3 = 'base=5'] = process.argv;
-const [, base = 5] = arg3.split('=');
+console.log(process.argv);
+console.log(argv);
+console.log('base: yargs', argv.base);
 
+// const [, , arg3 = 'base=5'] = process.argv;
+// const [, base = 5] = arg3.split('=');
 // const base = 5;
 
-createFile(base)
-  .then((fileName) => console.log(fileName, 'created'))
-  .catch((err) => console.log(err));
+// createFile(base)
+// .then((fileName) => console.log(fileName, 'created'))
+// .catch((err) => console.log(err));
