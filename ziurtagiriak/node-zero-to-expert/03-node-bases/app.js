ff88249ef34851c7ae65c35d4f94ccf165ff9187
Iñaki García <igardiet@gmail.com>
2023-05-28 22:52:16 +0200

    FEAT: argv.u (until) option

--- ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
@@ -4,19 +4,17 @@ require('colors');
 
 console.clear();
 
-createFile(argv.b, argv.l)
+createFile(argv.b, argv.l, argv.u)
   .then((fileName) => console.log(fileName.rainbow, 'created'))
   .catch((err) => console.log(err));
 
 // COMMANDS TO TEST THIS!
-// node app --help
-// node app -b 7 -l
+// node app --help        | help
+// node app -b 5          | base
+// node app -b 7 -l       | list
+// node app -b 2 -l -u=20 | until
 // ----------------
 
 // console.log(argv);
 // console.log(process.argv);
 // console.log('base: yargs', argv.b);
-
-// const [, , arg3 = 'base=5'] = process.argv;
-// const [, base = 5] = arg3.split('=');
-// const base = 5;
