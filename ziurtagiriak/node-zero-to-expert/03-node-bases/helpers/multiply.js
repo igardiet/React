549faf08cefe79957ebdeb636dbbe4f012b20a25
Iñaki García <igardiet@gmail.com>
2023-05-28 22:23:47 +0200

    FEAT: colors require; colors for styles in table structure

--- ziurtagiriak/node-zero-to-expert/03-node-bases/helpers/multiply.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/helpers/multiply.js
@@ -1,17 +1,18 @@
 const fs = require('fs');
+const colors = require('colors');
 
 const createFile = async (base = 5, list = false) => {
   try {
     let exit = '';
 
     for (let i = 1; i <= 10; i++) {
-      exit += `${base} * ${i} = ${base * i}\n`;
+      exit += `${base} ${'X'.cyan} ${i} ${'='.cyan} ${base * i}\n`;
     }
 
     if (list) {
-      console.log('===============');
-      console.log('Table of:', base);
-      console.log('===============');
+      console.log('==============='.red);
+      console.log('Table of:'.blue, colors.white(base));
+      console.log('==============='.red);
       console.log(exit);
     }
 
