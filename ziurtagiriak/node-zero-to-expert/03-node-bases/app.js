67faf7ef3dac78794444a29ab76f75d7a3d546dd
Iñaki García <igardiet@gmail.com>
2023-05-28 19:50:52 +0200

    FEAT: test commands; argv moved to ../config/yargs.js

--- ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
+++ ziurtagiriak/node-zero-to-expert/03-node-bases/app.js
@@ -1,31 +1,18 @@
 const { createFile } = require('./helpers/multiply');
-const argv = require('yargs')
-  .option('b', {
-    alias: 'base',
-    type: 'number',
-    demandOption: true,
-  })
-  .option('l', {
-    alias: 'list',
-    type: 'boolean',
-    demandOption: true,
-    default: false,
-  })
-  .check((argv, options) => {
-    if (isNaN(argv.b)) {
-      throw 'Base must be a number';
-    }
-    return true;
-  }).argv;
+const argv = require('./config/yargs');
 
 console.clear();
-
-console.log(argv);
 
 createFile(argv.b, argv.l)
   .then((fileName) => console.log(fileName, 'created'))
   .catch((err) => console.log(err));
 
+// COMMANDS TO TEST THIS!
+// node app --help
+// node app -b 7 -l
+// ----------------
+
+// console.log(argv);
 // console.log(process.argv);
 // console.log('base: yargs', argv.b);
 
