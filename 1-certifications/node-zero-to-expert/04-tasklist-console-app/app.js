8f42c03588a90716d5d0530fc68ef7a7af37abb7
Iñaki García <igardiet@gmail.com>
2023-05-31 20:44:17 +0200

    FEAT: main callback function

---
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/app.js
@@ -0,0 +1,12 @@
+require('colors');
+const { showMenu, pause } = require('./helpers/messages');
+
+console.clear();
+
+const main = () => {
+  console.log('Start');
+  showMenu();
+  //   pause()
+};
+
+main();
