faacfe93c1f8571a3b2d7249fc873f53f90e23cc
Iñaki García <igardiet@gmail.com>
2023-06-03 22:01:02 +0200

    FEAT: questions choices (7); pause async callback question then prompted

--- 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/inquirer.js
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/inquirer.js
@@ -6,7 +6,36 @@ const questions = [
     type: 'list',
     name: 'option',
     message: 'What would you like to do ?',
-    choices: ['opt1', 'opt2', 'opt3'],
+    choices: [
+      {
+        value: '1',
+        name: '1. Create task',
+      },
+      {
+        value: '2',
+        name: '2. List tasks',
+      },
+      {
+        value: '3',
+        name: '3. List completed tasks',
+      },
+      {
+        value: '4',
+        name: '4. List pending tasks',
+      },
+      {
+        value: '5',
+        name: '5. Complete task(s)',
+      },
+      {
+        value: '6',
+        name: '6. Delete task',
+      },
+      {
+        value: '0',
+        name: '0. Exit',
+      },
+    ],
   },
 ];
 
@@ -16,9 +45,23 @@ const inquirerMenu = async () => {
   console.log('Select an option'.red);
   console.log('===================\n'.green);
 
-  const opt = await inquirer.prompt(questions);
-  return opt;
+  const { option } = await inquirer.prompt(questions);
+  return option;
 };
+
+const pause = async () => {
+  const question = [
+    {
+      type: 'input',
+      name: 'enter',
+      message: `Press ${'enter'.red} to continue`,
+    },
+  ];
+  console.log('\n');
+  await inquirer.prompt(question);
+};
+
 module.exports = {
   inquirerMenu,
+  pause,
 };
