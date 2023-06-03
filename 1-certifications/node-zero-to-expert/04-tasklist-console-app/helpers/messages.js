41bec22bf390fdd11c4f62eeb3148136c8fe7384
Iñaki García <igardiet@gmail.com>
2023-06-03 21:12:49 +0200

    FEAT: return two new promises on showMenu and pause

--- 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/messages.js
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/messages.js
@@ -1,36 +1,42 @@
 require('colors');
 
 const showMenu = () => {
-  console.clear();
-  console.log('==================='.green);
-  console.log('Select an option'.red);
-  console.log('===================\n'.green);
-  console.log(`${'1.'.red} Create task`);
-  console.log(`${'2.'.red} List task`);
-  console.log(`${'3.'.red} List completed tasks`);
-  console.log(`${'4.'.red} List remaining tasks`);
-  console.log(`${'5.'.red} Complete task(s)`);
-  console.log(`${'6.'.red} Delete task`);
-  console.log(`${'0.'.red} Exit \n`);
+  return new Promise((resolve) => {
+    console.clear();
+    console.log('==================='.green);
+    console.log('Select an option'.red);
+    console.log('===================\n'.green);
+    console.log(`${'1.'.red} Create task`);
+    console.log(`${'2.'.red} List task`);
+    console.log(`${'3.'.red} List completed tasks`);
+    console.log(`${'4.'.red} List remaining tasks`);
+    console.log(`${'5.'.red} Complete task(s)`);
+    console.log(`${'6.'.red} Delete task`);
+    console.log(`${'0.'.red} Exit \n`);
 
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question('Select an option: ', (opt) => {
-    readline.close();
+    readline.question('Select an option: ', (opt) => {
+      readline.close();
+      resolve(opt);
+    });
   });
 };
 
 const pause = () => {
-  const readline = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
+  return new Promise((resolve) => {
+    const readline = require('readline').createInterface({
+      input: process.stdin,
+      output: process.stdout,
+    });
 
-  readline.question(`\nPress ${'ENTER'.red} to continue\n`, (opt) => {
-    readline.close();
+    readline.question(`\nPress ${'ENTER'.red} to continue\n`, (opt) => {
+      readline.close();
+      resolve();
+    });
   });
 };
 
