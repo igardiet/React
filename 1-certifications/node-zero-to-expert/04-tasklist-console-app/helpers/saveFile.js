a01b16e717b1c60cf64e8068570811c209b404e8
Iñaki García <igardiet@gmail.com>
2023-06-04 11:45:03 +0200

    feat: readDb callback data json parse of info

--- 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/saveFile.js
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/helpers/saveFile.js
@@ -1,9 +1,21 @@
 const fs = require('fs');
+const file = './db/data.json';
 
 const saveDb = (data) => {
-  const file = './db/data.json';
   fs.writeFileSync(file, JSON.stringify(data));
 };
+
+const readDb = () => {
+  if (!fs.existsSync(file)) {
+    return null;
+  }
+  const info = fs.readFileSync(file, { encoding: 'utf-8' });
+  const data = JSON.parse(info);
+  console.log(data);
+  return data;
+};
+
 module.exports = {
   saveDb,
+  readDb,
 };
