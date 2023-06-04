f97b68da4f671b61c273845042749f2083752d0e
Iñaki García <igardiet@gmail.com>
2023-06-04 11:44:21 +0200

    feat: tasksDb const to call readDb function

--- 1-certifications/node-zero-to-expert/04-tasklist-console-app/app.js
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/app.js
@@ -1,17 +1,23 @@
 require('colors');
 const { inquirerMenu, pause, readInput } = require('./helpers/inquirer');
-const { saveDb } = require('./helpers/saveFile');
+const { saveDb, readDb } = require('./helpers/saveFile');
 const Tasks = require('./models/Tasks');
 
 const main = async () => {
   let opt = '';
   const tasks = new Tasks();
 
+  const tasksDb = readDb();
+  if (tasksDb) {
+    //establish tasks
+  }
+  await pause();
+
   do {
-    opt = await inquirerMenu();
+    opt = await inquirerMenu(); // Prints menu
     switch (opt) {
       case '1':
-        const desc = await readInput('Description: ');
+        const desc = await readInput('Description: '); // Create option
         tasks.createTask(desc);
         break;
       case '2':
