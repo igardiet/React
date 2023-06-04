aa2edc50047b28efc85ef7137c84bf07aeb9ec23
Iñaki García <igardiet@gmail.com>
2023-06-04 10:35:57 +0200

    feat: createTask from task class by id

--- 1-certifications/node-zero-to-expert/04-tasklist-console-app/models/tasks.js
+++ 1-certifications/node-zero-to-expert/04-tasklist-console-app/models/tasks.js
@@ -1,7 +1,13 @@
+const Task = require('./task');
+
 class Tasks {
   _list = {};
   constructor() {
     this._list = {};
+  }
+  createTask(desc = '') {
+    const task = new Task(desc);
+    this._list[task.id] = task;
   }
 }
 module.exports = Tasks;
