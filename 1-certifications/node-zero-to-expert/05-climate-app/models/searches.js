a941376f967e16cf9e672cd65f60fb540690cffb
Iñaki García <igardiet@gmail.com>
2023-06-10 18:37:40 +0200

    feat: searches class

---
+++ 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
@@ -0,0 +1,13 @@
+class Searches {
+  history = ['Buenos Aires', 'Barcelona', 'Paris'];
+
+  constructor() {
+    // read db if exists
+  }
+  async city(place = '') {
+    // http petition
+    console.log(place);
+    return []; // return places
+  }
+}
+module.exports = Searches;
