cf6cb349ec66065be814534e68f1b827cc49dae1
Iñaki García <igardiet@gmail.com>
2023-06-10 18:08:28 +0200

    feat: else if main

---
+++ c/basics/elseIf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main()
+{
+    printf("Choose a menu option 1-4:\n");
+    printf("1. add a patient\n");
+    printf("2. view a patient\n");
+    printf("3. search patients\n");
+    printf("4. exit:\n");
+
+    int input;
+    scanf("%d", &input);
+
+    // if()
+    // {
+
+    // }
+    return 0;
+}
