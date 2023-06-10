37e9dcb86fd1fdb46c90cc4c04eacddd1bb22209
Iñaki García <igardiet@gmail.com>
2023-06-10 18:37:24 +0200

    feat: inquirerMenu switch on do

--- 1-certifications/node-zero-to-expert/05-climate-app/index.js
+++ 1-certifications/node-zero-to-expert/05-climate-app/index.js
@@ -1,10 +1,33 @@
 const { readInput, inquirerMenu, pause } = require('./helpers/inquirer');
+const Searches = require('./models/searches');
 
 const main = async () => {
+  const searches = new Searches();
   let opt;
+
   do {
     opt = await inquirerMenu();
-    console.log({ opt });
+    switch (opt) {
+      case 1:
+        // show message
+        const place = await readInput('City: ');
+        console.log(place);
+        // search places
+
+        // select place
+
+        // climate
+
+        // show results
+        console.log('\nCity information\n'.red);
+        console.log('City:');
+        console.log('Lat:');
+        console.log('Lng:');
+        console.log('Temperature:');
+        console.log('Min:');
+        console.log('Max:');
+        break;
+    }
 
     if (opt !== 0) await pause();
   } while (opt !== 0);
