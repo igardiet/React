e1cb1a1a72284927752bb6d04932cece8f5e212c
Iñaki García <igardiet@gmail.com>
2023-06-10 19:22:40 +0200

    feat: trycatch axios.get api

--- 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
+++ 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
@@ -1,13 +1,22 @@
+const axios = require('axios');
+
 class Searches {
   history = ['Buenos Aires', 'Barcelona', 'Paris'];
 
   constructor() {
     // read db if exists
   }
+
   async city(place = '') {
-    // http petition
-    console.log(place);
-    return []; // return places
+    try {
+      // http petition
+      // console.log('city', place);
+      const ans = await axios.get('https://reqres.in/api/users?page=2');
+      console.log(ans.data);
+      return [];
+    } catch (error) {
+      return [];
+    }
   }
 }
 module.exports = Searches;
