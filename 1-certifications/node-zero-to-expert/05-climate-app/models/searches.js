0a8658ef24901ecc9948d786cbb8d10f3d25b2f8
Iñaki García <igardiet@gmail.com>
2023-06-12 17:22:16 +0200

    feat: capitalizedHistory, addHistory, saveDB, readDB

--- 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
+++ 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
@@ -1,10 +1,20 @@
+const fs = require('fs');
 const axios = require('axios');
 
 class Searches {
-  history = ['Buenos Aires', 'Barcelona', 'Paris'];
+  history = [];
+  dbPath = './db/database.json';
 
   constructor() {
-    // read db if exists
+    this.readDB();
+  }
+
+  get capitalizedHistory() {
+    return this.history.map(place => {
+      let words = place.split(' ');
+      words = words.map(p => p[0].toUpperCase() + p.substring(1));
+      return words.join(' ');
+    });
   }
 
   get paramsMapbox() {
@@ -61,6 +71,28 @@ class Searches {
     } catch (error) {
       console.log(error);
     }
+  }
+
+  addHistory(place = '') {
+    if (this.history.includes(place.toLocaleLowerCase())) {
+      return;
+    }
+    this.history = this.history.splice(0, 4);
+    this.history.unshift(place.toLocaleLowerCase());
+    this.saveDB();
+  }
+  saveDB() {
+    const payload = {
+      history: this.history,
+    };
+    fs.writeFileSync(this.dbPath, JSON.stringify(payload));
+  }
+  readDB() {
+    if (!fs.existsSync(this.dbPath)) return;
+    const info = fs.readFileSync(this.dbPath, { encoding: 'utf-8' });
+    const data = JSON.parse(info);
+
+    this.history = data.history;
   }
 }
 module.exports = Searches;
