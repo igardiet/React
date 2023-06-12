be9f3a4aa01a1dd73e9a59867f19ac0c60393db4
Iñaki García <igardiet@gmail.com>
2023-06-12 15:38:46 +0200

    feat: get paramsWeather; climatePlace async function

--- 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
+++ 1-certifications/node-zero-to-expert/05-climate-app/models/searches.js
@@ -15,6 +15,14 @@ class Searches {
     };
   }
 
+  get paramsWeather() {
+    return {
+      appId: process.env.OPENWEATHER_KEY,
+      units: 'metric',
+      lang: 'en',
+    };
+  }
+
   async city(place = '') {
     try {
       const instance = axios.create({
@@ -23,11 +31,35 @@ class Searches {
       });
 
       const ans = await instance.get();
-      console.log(ans.data);
-
-      return [];
+      return ans.data.features.map(place => ({
+        id: place.id,
+        name: place.place_name,
+        lng: place.center[0],
+        lat: place.center[1],
+      }));
     } catch (error) {
       return [];
+    }
+  }
+
+  async climatePlace(lat, lon) {
+    try {
+      const instance = axios.create({
+        baseURL: `https://api.openweathermap.org/data/2.5/weather`,
+        params: { ...this.paramsWeather, lat, lon },
+      });
+
+      const ans = await instance.get();
+      const { weather, main } = ans.data;
+
+      return {
+        desc: weather[0].description,
+        min: main.temp_min,
+        max: main.temp_max,
+        temp: main.temp,
+      };
+    } catch (error) {
+      console.log(error);
     }
   }
 }
