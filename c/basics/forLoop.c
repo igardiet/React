bd0b6f3f9bff445a5ad06d08ed9384cdb362c1de
Iñaki García <igardiet@gmail.com>
2023-06-13 11:27:03 +0200

    feat: for loop icu

---
+++ c/basics/forLoop.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    // init
+    // comparison
+    // update
+
+    int max;
+    printf("Insert an integer: ");
+    scanf("%d", &max);
+
+    for (int i = 1; i <= max; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
