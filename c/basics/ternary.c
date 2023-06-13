37045d4b0f3b5111d1e3ec831874e33942a352b8
Iñaki García <igardiet@gmail.com>
2023-06-13 09:44:38 +0200

    feat: ternary operator condition

--- c/basics/ternary.c
+++ c/basics/ternary.c
@@ -2,5 +2,10 @@
 
 int main()
 {
+    double money = 25;
+    double cost = 25;
+
+    money >= cost ? printf("Affordable!\n") : printf("Not affordable, you need %lf more\n", cost - money);
+
     return 0;
 }
