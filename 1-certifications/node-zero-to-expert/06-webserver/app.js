95a8f2eeb0d779283b2d34c714b28072b4b3f037
Iñaki García <igardiet@gmail.com>
2023-06-13 13:59:30 +0200

    feat: express and handlebars routes

--- 1-certifications/node-zero-to-expert/06-webserver/app.js
+++ 1-certifications/node-zero-to-expert/06-webserver/app.js
@@ -2,16 +2,27 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
+app.set('view engine', 'hbs');
+
+app.use(express.static('public')); // Serve static content middleware
+
 app.get('/', (req, res) => {
-  res.send('Home route');
+  res.render('home', {
+    name: 'George Lucas',
+    title: 'Star Wars',
+  });
 });
 
-app.get('/categories', (req, res) => {
-  res.send('Categories route');
+app.get('/generic', (req, res) => {
+  res.sendFile(__dirname + '/public/generic.html');
+});
+
+app.get('/elements', (req, res) => {
+  res.sendFile(__dirname + '/public/elements.html');
 });
 
 app.get('*', (req, res) => {
-  res.send('404 | Page not found!');
+  res.sendFile(__dirname + '/public/404.html');
 });
 
 app.listen(port, () => {
