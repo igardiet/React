04d41ce0f9d47365c26dfacb098e17248250b061
Iñaki García <igardiet@gmail.com>
2023-06-14 13:07:17 +0200

    feat: nested while program

---
+++ c/basics/nestedWhile.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+    int i = 0, k;
+    while (i < 11)
+    {
+        int k = i;
+        i++;
+
+        while (k >= 0)
+        {
+            printf("%d ", k);
+            k--;
+        }
+        printf("\n");
+    }
+    return 0;
+}
