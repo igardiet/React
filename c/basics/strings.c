605169aa3f4900a7af148374c92570cbd5ccd2b0
Iñaki García <igardiet@gmail.com>
2023-06-14 19:44:19 +0200

    feat: count string length input program

---
+++ c/basics/strings.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+    printf("What is your favourite food?: ");
+    char favFood[50];
+    scanf("%49s", favFood);
+    printf("%s\n", favFood);
+
+    int charCount = strlen(favFood);
+
+    // int charCount = 0;
+    // while(favFood[charCount] != '\0')
+    // {
+    //     charCount++;
+    // }
+    printf("The character count is %d\n", charCount);
+    return 0;
+}
