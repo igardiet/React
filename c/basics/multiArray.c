61538b20baa2e5f71098baf23ab54fa27a6381f1
Iñaki García <igardiet@gmail.com>
2023-06-17 20:29:15 +0200

    refactor: void printArray function then called on main

--- c/basics/multiArray.c
+++ c/basics/multiArray.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+void printArray(int arr[], int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+}
+
 int main()
 {
     int const columns = 3;
@@ -9,10 +17,7 @@ int main()
         {64, 78, 89}};
     for (int i = 0; i < rows; i++)
     {
-        for (int j = 0; j < columns; j++)
-        {
-            printf("%d ", grades[i][j]);
-        }
+        printArray(grades[i], columns);
         printf("\n");
     }
     return 0;
