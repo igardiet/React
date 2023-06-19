8740c274615b0b89ec5b50415900b76fdd77b172
Iñaki García <igardiet@gmail.com>
2023-06-19 17:23:30 +0200

    mongoose connection to database on connectDB

--- 1-certifications/node-zero-to-expert/07-restserver/models/server.js
+++ 1-certifications/node-zero-to-expert/07-restserver/models/server.js
@@ -1,18 +1,24 @@
 const express = require('express');
 const cors = require('cors');
+const { dbConnection } = require('../database/config');
 
 class Server {
   constructor() {
     this.app = express();
-    this.port = process.env.PORT || 3000;
+    this.port = process.env.PORT || 3000;
     this.usersPath = '/api/users';
-    this.middlewares();
-    this.routes();
+    this.connectDB(); // Connection to database
+    this.middlewares(); // Middlewares
+    this.routes(); // App routes
+  }
+
+  async connectDB() {
+    await dbConnection();
   }
 
   middlewares() {
     this.app.use(cors()); // cors
-    this.app.use(express.json());  // Body parse
+    this.app.use(express.json()); // Body parse
     this.app.use(express.static('public')); // Public directory
   }
 
