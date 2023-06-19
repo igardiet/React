a47971dddc6b8bddf5ece5126ee9a4e3351bc14f
Iñaki García <igardiet@gmail.com>
2023-06-19 15:12:19 +0200

    feat: users routes

---
+++ 1-certifications/node-zero-to-expert/07-restserver/routes/users.js
@@ -0,0 +1,17 @@
+const { Router } = require('express');
+const router = Router();
+const {
+  getUsers,
+  postUsers,
+  putUsers,
+  patchUsers,
+  deleteUsers,
+} = require('../controllers/users');
+
+router.get('/', getUsers);
+router.post('/', postUsers);
+router.put('/', putUsers);
+router.patch('/', patchUsers);
+router.delete('/', deleteUsers);
+
+module.exports = router;
