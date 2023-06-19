326e4910535e9f678ae91c9099fe945f0bbbd786
Iñaki García <igardiet@gmail.com>
2023-06-19 23:32:06 +0200

    feat: check middleware if it's email syntax

--- 1-certifications/node-zero-to-expert/07-restserver/routes/users.js
+++ 1-certifications/node-zero-to-expert/07-restserver/routes/users.js
@@ -1,4 +1,5 @@
 const { Router } = require('express');
+const { check } = require('express-validator');
 const router = Router();
 const {
   getUsers,
@@ -9,7 +10,11 @@ const {
 } = require('../controllers/users');
 
 router.get('/', getUsers);
-router.post('/', postUsers);
+router.post(
+  '/',
+  [check('email', 'Email syntax is not valid!').isEmail()],
+  postUsers
+);
 router.put('/:id', putUsers);
 router.patch('/', patchUsers);
 router.delete('/', deleteUsers);
