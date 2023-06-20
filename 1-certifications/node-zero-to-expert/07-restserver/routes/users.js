75008b7ec19f3e079d21882d8cf59d9bee0abe57
Iñaki García <igardiet@gmail.com>
2023-06-20 15:24:02 +0200

    post check: name, password, email, email custom, role custom

--- 1-certifications/node-zero-to-expert/07-restserver/routes/users.js
+++ 1-certifications/node-zero-to-expert/07-restserver/routes/users.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const { check } = require('express-validator');
 const router = Router();
+const { validateFields } = require('../middlewares/validateFields');
+const {isValidRole, isEmailExistent} = require('../helpers/db-validators');
 const {
   getUsers,
   postUsers,
@@ -12,7 +14,17 @@ const {
 router.get('/', getUsers);
 router.post(
   '/',
-  [check('email', 'Email syntax is not valid!').isEmail()],
+  [
+    check('name', 'Name is mandatory').not().isEmpty(),
+    check('password', 'Password must be longer than 6 characters').isLength({
+      min: 6,
+    }),
+    check('email', 'Email syntax is not valid!').isEmail(),
+    check('email').custom(isEmailExistent),
+    // check('role', 'This role is not valid!').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    check('role').custom(isValidRole),
+    validateFields,
+  ],
   postUsers
 );
 router.put('/:id', putUsers);
