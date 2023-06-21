ab512d09b4a76753ef7b1a73361459e0fc1f73a5
Iñaki García <igardiet@gmail.com>
2023-06-21 13:51:02 +0200

    feat: User find and countDocs from async/await to promise; deleteUsers async callback

--- 1-certifications/node-zero-to-expert/07-restserver/controllers/users.js
+++ 1-certifications/node-zero-to-expert/07-restserver/controllers/users.js
@@ -4,9 +4,19 @@ const User = require('../models/user');
 
 const getUsers = async (req = request, res = response) => {
   const { limit = 5, from = 0 } = req.query;
-  const users = await User.find().skip(Number(from)).limit(Number(limit));
+  const query = { status: true };
 
+  // ASYNC/AWAIT
+  // const users = await User.find(query).skip(Number(from)).limit(Number(limit));
+  // const total = await User.countDocuments(query)
+
+  // PROMISE
+  const [total, users] = await Promise.all([
+    User.countDocuments(query),
+    User.find(query).skip(Number(from)).limit(Number(limit)),
+  ]);
   res.json({
+    total,
     users,
   });
 };
@@ -14,14 +24,11 @@ const getUsers = async (req = request, res = response) => {
 const postUsers = async (req, res = response) => {
   const { name, email, password, role } = req.body;
   const user = new User({ name, email, password, role });
-
   // PASSWORD ENCRYPTION  *!
   const salt = bcrypt.genSaltSync();
   user.password = bcrypt.hashSync(password.toString(), salt);
-
   // SAVE IN DB
   await user.save();
-
   res.json({
     user,
   });
@@ -30,26 +37,24 @@ const postUsers = async (req, res = response) => {
 const putUsers = async (req, res = response) => {
   const { id } = req.params;
   const { _id, password, google, ...rest } = req.body;
-
   if (password) {
     const salt = bcrypt.genSaltSync();
     rest.password = bcrypt.hashSync(password.toString(), salt);
   }
-
   const user = await User.findByIdAndUpdate(id, rest);
+  res.json(user);
+};
 
+const deleteUsers = async (req, res = response) => {
+  const { id } = req.params;
+  // const user = await User.findByIdAndDelete(id); // DELETE PHYSICALLY FROM DATABASTE
+  const user = await User.findByIdAndUpdate(id, { status: false });
   res.json(user);
 };
 
 const patchUsers = (req, res) => {
   res.json({
     msg: 'patch API - controller',
-  });
-};
-
-const deleteUsers = (req, res) => {
-  res.json({
-    msg: 'delete API - controller',
   });
 };
 
