ac166042edd7efebac20b332721d6a5f611ae72c
Iñaki García <igardiet@gmail.com>
2023-06-25 23:41:28 +0200

    obtainCategories and obtainCategory async callbacks

--- 1-certifications/node-zero-to-expert/07-restserver/controllers/categories.js
+++ 1-certifications/node-zero-to-expert/07-restserver/controllers/categories.js
@@ -1,6 +1,29 @@
 const { response } = require('express');
 const { Category } = require('../models');
 
+const obtainCategories = async (req, res = response) => {
+  const { limit = 5, from = 0 } = req.query;
+  const query = { status: true };
+
+  const [total, categories] = await Promise.all([
+    Category.countDocuments(query),
+    Category.find(query)
+      .populate('user', 'name')
+      .skip(Number(from))
+      .limit(Number(limit)),
+  ]);
+  res.json({
+    total,
+    categories,
+  });
+};
+
+const obtainCategory = async (req, res = response) => {
+  const { id } = req.params;
+  const category = await Category.findById(id).populate('user', 'name');
+  res.json(category);
+};
+
 const createCategory = async (req, res = response) => {
   const name = req.body.name.toUpperCase();
   const categoryDB = await Category.findOne({ name });
@@ -17,6 +40,9 @@ const createCategory = async (req, res = response) => {
   await category.save();
   res.status(201).json(category);
 };
+
 module.exports = {
+  obtainCategories,
+  obtainCategory,
   createCategory,
 };
