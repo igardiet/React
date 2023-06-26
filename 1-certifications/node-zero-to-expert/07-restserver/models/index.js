fa3ceda1b83cb144af0ddaf1ec3e585cbc4418bd
Iñaki García <igardiet@gmail.com>
2023-06-26 17:42:50 +0200

    Product and User require export

--- 1-certifications/node-zero-to-expert/07-restserver/models/index.js
+++ 1-certifications/node-zero-to-expert/07-restserver/models/index.js
@@ -1,11 +1,13 @@
 const Category = require('./category');
+const Product = require('./product');
 const Role = require('./role');
-const User = require('./user');
 const Server = require('./server');
+const User = require('./user');
 
 module.exports = {
   Category,
+  Product,
   Role,
-  User,
   Server,
+  User,
 };
