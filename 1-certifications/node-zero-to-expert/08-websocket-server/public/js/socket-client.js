cabf30cc8bcfc6f7f505b806dbe76e6426cc3375
Iñaki García <igardiet@gmail.com>
2023-07-02 16:18:30 +0200

    socket.on payload; socket.emit id;

--- 1-certifications/node-zero-to-expert/08-websocket-server/public/js/socket-client.js
+++ 1-certifications/node-zero-to-expert/08-websocket-server/public/js/socket-client.js
@@ -6,15 +6,19 @@ const btnSend = document.querySelector('#btnSend');
 const socket = io();
 
 socket.on('connect', () => {
-  console.log('Connected');
+  // console.log('Connected');
   lblOffline.style.display = 'none';
   lblOnline.style.display = '';
 });
 
 socket.on('disconnect', () => {
-  console.log('Disconnected');
+  // console.log('Disconnected');
   lblOffline.style.display = '';
   lblOnline.style.display = 'none';
+});
+
+socket.on('send-message', payload => {
+  console.log(payload);
 });
 
 btnSend.addEventListener('click', () => {
@@ -24,5 +28,7 @@ btnSend.addEventListener('click', () => {
     id: '123ABC',
     date: new Date().getTime(),
   };
-  socket.emit('send-message', payload);
+  socket.emit('send-message', payload, id => {
+    console.log('From server', id);
+  });
 });
