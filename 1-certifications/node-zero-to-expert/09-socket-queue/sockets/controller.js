887578bd8cc223bf7921939745233a4ab93becd2
igardiet <igardiet@gmail.com>
2023-08-26 18:56:59 +0200

    feat: ticket control class instance to socket controller

--- 1-certifications/node-zero-to-expert/09-socket-queue/sockets/controller.js
+++ 1-certifications/node-zero-to-expert/09-socket-queue/sockets/controller.js
@@ -1,9 +1,36 @@
-const socketController = socket => {
-  socket.on('send-message', (payload, callback) => {
-    const id = 123456789;
-    callback(id);
+const TicketControl = require('../models/ticketControl');
 
-    socket.broadcast.emit('send-message', payload);
+const ticketControl = new TicketControl();
+
+const socketController = socket => {
+  socket.emit('last-ticket', ticketControl.last);
+  socket.on('next-ticket', (payload, callback) => {
+    const next = ticketControl.next();
+    callback(next);
+  });
+
+  socket.on('attend-ticket', ({ desktop }, callback) => {
+    if (!desktop) {
+      return callback({
+        ok: false,
+        msg: 'Desktop is mandatory',
+      });
+    }
+    const ticket = ticketControl.attendTicket(desktop);
+
+    // TODO: Notify change in last4
+
+    if (!ticket) {
+      callback({
+        ok: false,
+        msg: 'there are no more pending tickets',
+      });
+    } else {
+      callback({
+        ok: true,
+        ticket,
+      });
+    }
   });
 };
 
