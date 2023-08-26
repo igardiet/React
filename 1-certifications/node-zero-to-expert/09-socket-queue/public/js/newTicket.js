5448fe5a508a14d45f53cf19d2afce83d165f687
igardiet <igardiet@gmail.com>
2023-08-26 18:56:23 +0200

    feat: ticket socket connection

--- 1-certifications/node-zero-to-expert/09-socket-queue/public/js/newTicket.js
+++ 1-certifications/node-zero-to-expert/09-socket-queue/public/js/newTicket.js
@@ -1,1 +1,22 @@
-console.log('New HTML ticket');
+const lblNewTicket = document.querySelector('#lblNewTicket');
+const btnCreate = document.querySelector('button');
+
+const socket = io();
+
+socket.on('connect', () => {
+  btnCreate.disabled = false;
+});
+
+socket.on('disconnect', () => {
+  btnCreate.disabled = true;
+});
+
+socket.on('last-ticket', last => {
+  lblNewTicket.innerText = `Ticket ${last}`;
+});
+
+btnCreate.addEventListener('click', () => {
+  socket.emit('next-ticket', null, ticket => {
+    lblNewTicket.innerText = ticket;
+  });
+});
