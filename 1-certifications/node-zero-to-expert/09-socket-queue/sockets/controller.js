a0a8f07bc30f5f65195876492e9ec68e38f73d99
igardiet <igardiet@gmail.com>
2023-09-01 18:58:17 +0200

    feat: socket emitters on current-state and pending-tickets

--- 1-certifications/node-zero-to-expert/09-socket-queue/sockets/controller.js
+++ 1-certifications/node-zero-to-expert/09-socket-queue/sockets/controller.js
@@ -5,41 +5,48 @@ const ticketControl = new TicketControl();
 const socketController = socket =>
 {
   socket.emit( 'last-ticket', ticketControl.last );
+  socket.emit( 'current-state', ticketControl.last4 );
+  socket.emit( 'pending-tickets', ticketControl.tickets.length );
+
   socket.on( 'next-ticket', ( payload, callback ) =>
   {
     const next = ticketControl.next();
     callback( next );
+    socket.broadcast.emit( 'pending-tickets', ticketControl.tickets.length );
   } );
 
   socket.on( 'attend-ticket', ( { desktop }, callback ) =>
   {
     if ( !desktop )
     {
-      return callback( {
-        ok: false,
-        msg: 'Desktop is mandatory',
-      } );
+      return callback(
+        {
+          ok: false,
+          msg: 'Desktop is mandatory',
+        } );
     }
     const ticket = ticketControl.attendTicket( desktop );
 
-    // TODO: Notify change in last4
+    socket.broadcast.emit( 'current-state', ticketControl.last4 );
+    socket.emit( 'pending-tickets', ticketControl.tickets.length );
+    socket.broadcast.emit( 'pending-tickets', ticketControl.tickets.length );
 
     if ( !ticket )
     {
-      callback( {
-        ok: false,
-        msg: 'there are no more pending tickets',
-      } );
+      callback(
+        {
+          ok: false,
+          msg: 'there are no more pending tickets',
+        } );
     } else
     {
-      callback( {
-        ok: true,
-        ticket,
-      } );
+      callback(
+        {
+          ok: true,
+          ticket,
+        } );
     }
   } );
 };
 
-module.exports = {
-  socketController,
-};
+module.exports = { socketController };
