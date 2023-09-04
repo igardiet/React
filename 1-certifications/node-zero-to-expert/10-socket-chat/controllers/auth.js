fd7a674596a38b93b5e6aaefbdf2729e99671754
igardiet <igardiet@gmail.com>
2023-09-04 23:43:49 +0200

    feat: renewToken controller

--- 1-certifications/node-zero-to-expert/10-socket-chat/controllers/auth.js
+++ 1-certifications/node-zero-to-expert/10-socket-chat/controllers/auth.js
@@ -86,4 +86,13 @@ const googleSignIn = async ( req, res = response ) =>
   }
 };
 
-module.exports = { login, googleSignIn };
+const renewToken = async ( req, res = response ) =>
+{
+  const { user } = req;
+
+  const token = await generateJWT( user.id );
+
+  res.json( { user, token } );
+};
+
+module.exports = { login, googleSignIn, renewToken };
