772f6d2cec5d8d9913487b9ca48d7d337f2dd3b3
igardiet <igardiet@gmail.com>
2023-09-07 12:05:10 +0200

    feat: chat messages instance to connect and disconnect user

--- 1-certifications/node-zero-to-expert/10-socket-chat/sockets/controller.js
+++ 1-certifications/node-zero-to-expert/10-socket-chat/sockets/controller.js
@@ -1,12 +1,22 @@
 const { Socket } = require( 'socket.io' );
 const { findJWT } = require( "../helpers" );
+const ChatMessages = require( "../models/chatMessages" );
 
-const socketController = async ( socket = new Socket() ) =>
+const chatMessages = new ChatMessages();
+
+const socketController = async ( socket = new Socket(), io ) =>
 {
     const user = await findJWT( socket.handshake.headers['x-token'] );
     if ( !user ) return socket.disconnect();
 
-    console.log( `${user.name} has connected` );
+    chatMessages.connectUser( user );
+    io.emit( 'active-users', chatMessages.usersArray );
+
+    socket.on( 'disconnect', () =>
+    {
+        chatMessages.disconnectUser( user.id );
+        io.emit( 'active-users', chatMessages.usersArray );
+    } );
 };
 
 module.exports = { socketController };
