8f9336d55c8146da537bfead1b1ebe24f8bc625f
igardiet <igardiet@gmail.com>
2023-09-10 00:52:31 +0200

    feat: receive function

---
+++ solidity/contract-18.sol
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// Receive function: ether directly to a smart contract
+contract Contract18 {
+    receive() external payable {}
+}
