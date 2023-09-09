4846482a5ce50438d664b185a9930eed03785fb1
igardiet <igardiet@gmail.com>
2023-09-09 18:59:47 +0200

    feat: constructor

---
+++ solidity/contract-7.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract Contract7 {
+    string public name;
+
+    // The constructor function runs once whenever the contract is initialized or put on the blockchain
+    constructor(string memory _name) {
+        name = _name;
+    }
+}
