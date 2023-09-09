bc5dcb1cb821b0827f09653974f1653b2647e2ce
igardiet <igardiet@gmail.com>
2023-09-09 16:47:49 +0200

    feat: function privacy types

---
+++ solidity/contract-4.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.8.0;
+
+contract Contract4 {
+    uint public count;
+
+    // public function can be called outside the smart contract
+    function increment1() public {
+        count = count + 1;
+    }
+
+    // public function can be called by another function
+    function increment2() public {
+        increment1();
+    }
+
+    // private function can't be called outside the smart contract
+    function increment3() private {
+        count = count + 1;
+    }
+
+    // private function can be called inside of another function of the same contract
+    function increment4() public {
+        increment3();
+    }
+
+    // external function can only be called outside the smart contract
+    function increment5() external {
+        count = count + 1;
+    }
+
+    // internal function can only be called inside the same smart contract
+    function increment6() internal {
+        count = count + 1;
+    }
+
+    function increment7() public {
+        increment6();
+    }
+}
