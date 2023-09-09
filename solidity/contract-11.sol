90abe5a2dde847995843aeac07324f3416874c6d
igardiet <igardiet@gmail.com>
2023-09-09 22:25:25 +0200

    feat: array declarations

---
+++ solidity/contract-11.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.0;
+
+contract Contract11 {
+    uint[] public array1 = [1, 2, 3];
+    uint[] public array2;
+    uint[10] public array3;
+    string[] public array4 = ["watermelon", "orange", "peach"];
+    string[] public array5;
+    string[10] public array6;
+}
