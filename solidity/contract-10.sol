612ccb6dd37cc6782b600b84b4e704c754a377c9
igardiet <igardiet@gmail.com>
2023-09-09 22:25:06 +0200

    feat: conditionals

---
+++ solidity/contract-10.sol
@@ -0,0 +1,21 @@
+pragma solidity ^0.8.0;
+
+// Conditionals
+contract Contract10 {
+    function evenOrOdd1(uint x) public pure returns (string memory) {
+        if (x % 2 == 0) {
+            return "even";
+        } else {
+            return "odd";
+        }
+    }
+
+    function evenOrOdd2(uint x) public pure returns (string memory) {
+        if (x % 2 == 0) return "even";
+        return "odd";
+    }
+
+    function evenOrOdd3(uint x) public pure returns (string memory) {
+        return x % 2 == 0 ? "even" : "odd";
+    }
+}
