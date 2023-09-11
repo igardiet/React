99e512b670ed37e62a9b85c62c40d28980c0ca67
igardiet <igardiet@gmail.com>
2023-09-11 15:03:27 +0200

    feat: examples of data types

---
+++ solidity/practice/contract-30.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// Data types examples
+contract Contract30 {
+    string public myString = "String";
+    bool public myBool = true;
+    int public myInt = -1;
+    uint public myUint = 1;
+    uint8 public myUint8 = 8;
+    uint256 public myUint256 = 99999;
+}
