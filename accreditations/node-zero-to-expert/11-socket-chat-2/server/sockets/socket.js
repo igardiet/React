c141daaec7a74a22ce5505d53a29fabb5e3f7f4e
igardiet <igardiet@gmail.com>
2023-09-16 14:10:59 +0200

    feat: room data on client enterChat

--- accreditations/node-zero-to-expert/11-socket-chat-2/server/sockets/socket.js
+++ accreditations/node-zero-to-expert/11-socket-chat-2/server/sockets/socket.js
@@ -8,30 +8,32 @@ io.on( 'connection', ( client ) =>
 {
     client.on( 'enterChat', ( data, callback ) =>
     {
-        if ( !data.name )
+        if ( !data.name || !data.room )
         {
             return callback
                 ( {
                     error: true,
-                    message: 'Name is mandatory'
+                    message: 'Name/Room are mandatory'
                 } );
         }
-        let people = users.addPerson( client.id, data.name );
-        client.broadcast.emit( 'personList', users.getPeople() );
-        callback( people );
+        client.join( data.room );
+        users.addPerson( client.id, data.name, data.room );
+        client.broadcast.to( data.room ).emit( 'personList', users.getPeopleByRoom( data.room ) );
+        callback( users.getPeopleByRoom( data.room ) );
     } );
 
     client.on( 'createMessage', ( data ) =>
     {
         let person = users.getPerson( client.id );
         let message = createMessage( person.name, data.message );
-        client.broadcast.emit( 'createMessage', message );
+        client.broadcast.to( person.room ).emit( 'createMessage', message );
     } );
 
     client.on( 'disconnect', () =>
     {
         let deletedPerson = users.deletePerson( client.id );
-        client.broadcast.emit( 'createMessage', createMessage( 'Admin', `${deletedPerson.name} has left the chat` ) );
-        client.broadcast.emit( 'personList', users.getPeople() );
+       
     } );
+
+    
 } );
