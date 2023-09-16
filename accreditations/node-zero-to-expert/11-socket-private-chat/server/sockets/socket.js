dae580baad684b6ac9f2a9c1de09f95513f46535
igardiet <igardiet@gmail.com>
2023-09-16 21:50:03 +0200

    feat: createMessage emit by admin "user joined the chat"

--- accreditations/node-zero-to-expert/11-socket-private-chat/server/sockets/socket.js
+++ accreditations/node-zero-to-expert/11-socket-private-chat/server/sockets/socket.js
@@ -19,6 +19,7 @@ io.on( 'connection', ( client ) =>
         client.join( data.room );
         users.addPerson( client.id, data.name, data.room );
         client.broadcast.to( data.room ).emit( 'personList', users.getPeopleByRoom( data.room ) );
+        client.broadcast.to( data.room ).emit( 'createMessage', createMessage( 'Admin', `${data.name} joined the chat` ) );
         callback( users.getPeopleByRoom( data.room ) );
     } );
 
