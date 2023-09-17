fbeee068e0d412cea65bbf41025bea7ed5b20062
igardiet <igardiet@gmail.com>
2023-09-17 15:28:34 +0200

    feat: users paths and routes method then called on constructor

--- accreditations/node-zero-to-expert/12-ts-rest-server/models/server.ts
+++ accreditations/node-zero-to-expert/12-ts-rest-server/models/server.ts
@@ -1,14 +1,25 @@
 import express, { Application } from 'express';
+import userRoutes from '../routes/users'
 
 class Server
 {
     private app: Application;
     private port: string;
+    private paths =
+        {
+            users: '/api/users'
+        }
 
     constructor()
     {
         this.app = express();
         this.port = process.env.PORT || '8000';
+        this.routes();
+    }
+
+    routes ()
+    {
+        this.app.use( this.paths.users, userRoutes )
     }
 
     listen ()
