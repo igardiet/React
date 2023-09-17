320819c8e6c57222c56bed46fd4a733d24355083
igardiet <igardiet@gmail.com>
2023-09-17 13:09:51 +0200

    feat: app config

--- accreditations/node-zero-to-expert/12-ts-rest-server/app.ts
+++ accreditations/node-zero-to-expert/12-ts-rest-server/app.ts
@@ -1,3 +1,8 @@
-export const name = 'Richard'
+import dotenv from 'dotenv';
+import Server from './models/server';
 
-console.log(name);
+dotenv.config();
+
+const server = new Server();
+
+server.listen();
