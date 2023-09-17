eff69492c80faf6d224b8d7f2fa9c7d6ca78f616
igardiet <igardiet@gmail.com>
2023-09-17 22:52:52 +0200

    feat: dbConnection method

--- accreditations/node-zero-to-expert/12-ts-rest-server/models/server.ts
+++ accreditations/node-zero-to-expert/12-ts-rest-server/models/server.ts
@@ -1,6 +1,7 @@
 import express, { Application } from 'express';
-import userRoutes from '../routes/users'
 import cors from 'cors';
+import userRoutes from '../routes/users'
+import db from '../db/connection';
 
 class Server
 {
@@ -15,8 +16,22 @@ class Server
     {
         this.app = express();
         this.port = process.env.PORT || '8000';
+
+        this.dbConnection();
         this.middlewares();
         this.routes();
+    }
+
+    async dbConnection ()
+    {
+        try
+        {
+            await db.authenticate();
+            console.log( 'Database online' );
+        } catch ( error: any )
+        {
+            throw new Error( error );
+        }
     }
 
     middlewares ()
