35b5a36f138d9102c05d912801a6abaf573063ad
igardiet <igardiet@gmail.com>
2023-09-24 00:31:59 +0200

    feat: exercise class

---
+++ accreditations/java-masterclass/Challenge.java
@@ -0,0 +1,18 @@
+public class Challenge
+{
+    public static void main(String[] args)
+    {
+        double myFirstValue = 20.00;
+        double mySecondValue = 80.00;
+        double myValuesTotal = (myFirstValue + mySecondValue) * 100.00;
+        System.out.println("myValuesTotal: " + myValuesTotal);
+        double theRemainder = myValuesTotal % 40.00;
+        System.out.println("theRemainder: " + theRemainder);
+        boolean isNoRemainder = (theRemainder == 0) ? true : false;
+        System.out.println("isNoRemainder: " + isNoRemainder);
+        if (!isNoRemainder)
+        {
+            System.out.println("Got some remainder");
+        }
+    }
+}
