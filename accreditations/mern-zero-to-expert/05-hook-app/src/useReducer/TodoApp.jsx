76c8d7f17bd9384cd619cb4f8bd1b3d59a702fda
igardiet <igardiet@gmail.com>
2023-10-03 23:16:11 +0200

    feat: initialState array and TodoApp component

---
+++ accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoApp.jsx
@@ -0,0 +1,33 @@
+import { useReducer } from "react";
+import { todoReducer } from "./todoReducer";
+
+const initialState =
+    [
+        {
+            id: new Date().getTime(),
+            description: 'Task nº1',
+            done: false
+        },
+        {
+            id: new Date().getTime() * 3,
+            description: 'Task nº2',
+            done: false
+        }
+    ];
+
+export const TodoApp = () =>
+{
+    const [todos, dispatch] = useReducer( todoReducer, initialState );
+
+    return (
+        <>
+            <h1>TodoApp</h1>
+            <hr />
+            <ul>
+                <li>Item 1</li>
+                <li>Item 2</li>
+                <li>Item 3</li>
+            </ul>
+        </>
+    );
+};
