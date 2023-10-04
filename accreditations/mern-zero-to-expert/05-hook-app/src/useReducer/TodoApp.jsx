f94747e349df029f88c9028da708e03a487e6c33
igardiet <igardiet@gmail.com>
2023-10-04 12:36:38 +0200

    feat: return TodoApp html shell

--- accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoApp.jsx
+++ accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoApp.jsx
@@ -21,13 +21,26 @@ export const TodoApp = () =>
 
     return (
         <>
-            <h1>TodoApp</h1>
+            <h1>TodoApp: 10 |<small>Pending: 2</small></h1>
             <hr />
-            <ul>
-                <li>Item 1</li>
-                <li>Item 2</li>
-                <li>Item 3</li>
-            </ul>
+            <div className="row">
+                <div className="col-7">
+                    <ul className="list-group">
+                        <li className="list-group-item d-flex justify-content-between">
+                            <span className="align-self-center">Item 1</span>
+                            <button className="btn btn-danger">Delete</button>
+                        </li>
+                    </ul>
+                </div>
+                <div className="col-5">
+                    <h4>Add Task</h4>
+                    <hr />
+                    <form>
+                        <input type="text" className="form-control" placeholder="What is there to do ?" />
+                        <button type="submit" className="btn btn-outline-primary mt-1">Add</button>
+                    </form>
+                </div>
+            </div>
         </>
     );
 };
