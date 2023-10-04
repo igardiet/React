e66ffb94d3fc6ee8b11be62dd9494e2bc68a68d7
igardiet <igardiet@gmail.com>
2023-10-04 21:26:33 +0200

    feat: TodoAdd component

---
+++ accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoAdd.jsx
@@ -0,0 +1,38 @@
+
+
+export const TodoAdd = ( { onNewTodo } ) =>
+{
+    const { description, onInputChange, onResetForm } = useForm( { description: '' } );
+    const onFormSubmit = ( event ) =>
+    {
+        event.preventDefault();
+        if ( description.length <= 1 ) return;
+
+        const newTodo =
+        {
+            id: new Date().getTime(),
+            done: false,
+            description
+        };
+
+        onNewTodo( newTodo );
+    };
+
+    return (
+        <form onSubmit={onFormSubmit}>
+            <input
+                type="text"
+                className="form-control"
+                placeholder="What is there to do ?"
+                name="description"
+                value={description}
+                onChange={onInputChange}
+            />
+            <button
+                type="submit"
+                className="btn btn-outline-primary
+                 mt-1"
+            >Add</button>
+        </form>
+    );
+};
