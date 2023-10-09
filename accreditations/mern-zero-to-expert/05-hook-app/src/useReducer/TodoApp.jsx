e32ef0e385959fbb2c58c30775e031391f4d8dec
igardiet <igardiet@gmail.com>
2023-10-10 00:00:57 +0200

    feat: logic moved to useTodos hook

--- accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoApp.jsx
+++ accreditations/mern-zero-to-expert/05-hook-app/src/useReducer/TodoApp.jsx
@@ -1,58 +1,14 @@
-import { useEffect, useReducer } from "react";
-import { todoReducer } from "./todoReducer";
-import { TodoList } from "./TodoList";
-import { TodoAdd } from "./TodoAdd";
-
-const initialState =
-    [
-
-    ];
-
-const init = () =>
-{
-    return JSON.parse( localStorage.getItem( 'todos' ) ) || [];
-};
+import { TodoList } from './TodoList';
+import { TodoAdd } from './TodoAdd';
+import { useTodos } from '../hooks';
 
 export const TodoApp = () =>
 {
-    const [todos, dispatch] = useReducer( todoReducer, initialState, init );
-
-    useEffect( () =>
-    {
-        localStorage.setItem( 'todos', JSON.stringify( todos ) );
-    }, [todos] );
-
-    const handleNewTodo = ( todo ) =>
-    {
-        const action =
-        {
-            type: 'Add Todo',
-            payload: todo
-        };
-        dispatch( action );
-    };
-
-    const handleDeleteTodo = ( id ) =>
-    {
-        dispatch(
-            {
-                type: 'Remove Todo',
-                payload: id
-            } );
-    };
-
-    const handleToggleTodo = ( id ) =>
-    {
-        dispatch(
-            {
-                type: 'Toggle Todo',
-                payload: id
-            } );
-    };
+    const { todos, todosCount, pendingTodosCount, handleDeleteTodo, handleToggleTodo, handleNewTodo } = useTodos();
 
     return (
         <>
-            <h1>TodoApp: 10 |<small>Pending: 2</small></h1>
+            <h1>TodoApp: {todosCount} | <small>Pending: {pendingTodosCount}</small></h1>
             <hr />
             <div className="row">
                 <div className="col-7">
