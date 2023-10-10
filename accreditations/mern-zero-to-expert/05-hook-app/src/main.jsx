64df17694e110543f2b0b08b7eca2cb8c2bfa359
igardiet <igardiet@gmail.com>
2023-10-10 17:23:24 +0200

    feat: createBrowserRouter routes set on router variable

--- accreditations/mern-zero-to-expert/05-hook-app/src/main.jsx
+++ accreditations/mern-zero-to-expert/05-hook-app/src/main.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Navigate, RouterProvider, createBrowserRouter } from "react-router-dom";
+import { MainApp } from "./useContext/MainApp";
+import { HomePage } from "./useContext/HomePage";
+import { AboutPage } from "./useContext/AboutPage";
+import { LoginPage } from "./useContext/LoginPage";
 import './index.css';
 // import { CounterApp } from './useState/CounterApp';
 // import { HooksApp } from './HooksApp';
@@ -14,6 +19,37 @@ import './index.css';
 // import { CallbackHook } from './memos/CallbackHook';
 // import { Parent } from './homework-memo/Parent';
 // import './useReducer/intro-reducer';
-import { TodoApp } from "./useReducer/TodoApp";
+// import { TodoApp } from "./useReducer/TodoApp";
 
-ReactDOM.createRoot( document.getElementById( 'root' ) ).render( <TodoApp /> );
+const router = createBrowserRouter(
+    [
+        {
+            path: '/',
+            element: <MainApp />,
+            children:
+                [
+                    {
+                        path: '/',
+                        element: <HomePage />
+                    },
+                    {
+                        path: 'about',
+                        element: <AboutPage />
+                    },
+                    {
+                        path: 'login',
+                        element: <LoginPage />
+                    },
+                    {
+                        path: '*',
+                        element: <Navigate to='/' replace />
+                    },
+                ]
+        }
+    ]
+);
+
+ReactDOM.createRoot( document.getElementById( 'root' ) ).render
+    (
+        <RouterProvider router={router} />
+    );
