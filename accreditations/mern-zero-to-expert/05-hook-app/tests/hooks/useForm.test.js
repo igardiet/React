b4684c61b14178dd932d6322cec2f4b36115063e
igardiet <igardiet@gmail.com>
2023-10-11 15:37:07 +0200

    feat: useForm jest tests

---
+++ accreditations/mern-zero-to-expert/05-hook-app/tests/hooks/useForm.test.js
@@ -0,0 +1,56 @@
+const { renderHook, act } = require( "@testing-library/react" );
+const { useForm } = require( "../../src/hooks/useForm" );
+
+describe( 'Tests in useFrom', () =>
+{
+    const initialForm =
+    {
+        name: 'Ignatius',
+        email: 'ignatius@gmail.com'
+    };
+
+    test( 'It must return default values', () =>
+    {
+        const { result } = renderHook( () => useForm( initialForm ) );
+
+        expect( result.current ).toEqual(
+            {
+                name: initialForm.name,
+                email: initialForm.email,
+                formState: initialForm,
+                onInputChange: expect.any( Function ),
+                onResetForm: expect.any( Function )
+            } );
+    } );
+
+    test( 'It must change the name of the form', () =>
+    {
+        const newValue = 'John';
+        const { result } = renderHook( () => useForm( initialForm ) );
+        const { onInputChange } = result.current;
+
+        act( () =>
+        {
+            onInputChange( { target: { name: 'name', value: newValue } } );
+        } );
+
+        expect( result.current.name ).toBe( newValue );
+        expect( result.current.formState.name ).toBe( newValue );
+    } );
+
+    test( 'it must reset the form', () =>
+    {
+        const newValue = 'John';
+        const { result } = renderHook( () => useForm( initialForm ) );
+        const { onInputChange, onResetForm } = result.current;
+
+        act( () =>
+        {
+            onInputChange( { target: { name: 'name', value: newValue } } );
+            onResetForm();
+        } );
+
+        expect( result.current.name ).toBe( initialForm.name );
+        expect( result.current.formState.name ).toBe( initialForm.name );
+    } );
+} );
