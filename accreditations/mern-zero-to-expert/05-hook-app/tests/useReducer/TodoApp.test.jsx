55a14707fff5679279e6e3378c21fd14daed0f7c
igardiet <igardiet@gmail.com>
2023-10-16 22:03:27 +0200

    chore: double quotes changed for single ones

--- accreditations/mern-zero-to-expert/05-hook-app/tests/useReducer/TodoApp.test.jsx
+++ accreditations/mern-zero-to-expert/05-hook-app/tests/useReducer/TodoApp.test.jsx
@@ -1,6 +1,6 @@
-import { render, screen } from "@testing-library/react";
-import { useTodos } from "../../src/hooks";
-import { TodoApp } from "../../src/useReducer/TodoApp";
+import { render, screen } from '@testing-library/react';
+import { useTodos } from '../../src/hooks';
+import { TodoApp } from '../../src/useReducer/TodoApp';
 
 jest.mock( '../../src/hooks/useTodos' );
 
